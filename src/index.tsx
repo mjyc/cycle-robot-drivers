04fef0532c7f667efe9dc6b17bd9553220fd93ea
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-08-04 11:49:52 -0700

    Return UtteranceSource that is modeled after MainDOMSource

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import Snabbdom from 'snabbdom-pragma';
 import xs from 'xstream';
-import fromEvent from 'xstream/extra/fromEvent'
 import delay from 'xstream/extra/delay'
 import {run} from '@cycle/run';
 import {makeDOMDriver} from '@cycle/dom';
@@ -8,41 +7,22 @@ import {makeDOMDriver} from '@cycle/dom';
 import {makeSpeechSynthesisDriver} from './speech_synthesis'
 
 function main(sources) {
-
   const vdom$ = xs.of((<div>Hello world!</div>));
-
-  // Approach 1
-  sources.SpeechSynthesis.addListener({
-    next: utterance => {
-      const start$ = fromEvent(utterance, 'start');
-      start$.addListener({next: start => console.log('start', start)});
-      const error$ = fromEvent(utterance, 'error');
-      error$.addListener({next: error => console.log('error', error)});
-      const end$ = fromEvent(utterance, 'end');
-      end$.addListener({next: end => console.log('end', end)});
-    }
-  });
-
-  // Approach 2: does not work
-  const speechSynthesisEvent$ = xs.merge(
-    sources.SpeechSynthesis.map(utterance => {
-      return fromEvent(utterance, 'end');
-    }).flatten(),
-    sources.SpeechSynthesis.map(utterance => {
-      return fromEvent(utterance, 'error');
-    }).flatten(),
-    sources.SpeechSynthesis.map(utterance => {
-      return fromEvent(utterance, 'start');
-    }).flatten(),
-  );
-
-  speechSynthesisEvent$.addListener({
-    next: event => console.warn('event', event)  // use "warn" to highlight differences
-  });
-
   const synth$ = xs.create();
   setTimeout(() => {synth$.shamefullySendNext(new SpeechSynthesisUtterance('Hello world'))}, 1);
+  setTimeout(() => {synth$.shamefullySendNext(null)}, 501);
   setTimeout(() => {synth$.shamefullySendNext(new SpeechSynthesisUtterance('Jello world'))}, 1001);
+
+  sources.SpeechSynthesis.events('start').addListener({
+    next: data => console.log('start', data),
+  });
+  sources.SpeechSynthesis.events('end').addListener({
+    next: data => console.log('end', data),
+  });
+  sources.SpeechSynthesis.events('error').addListener({
+    next: data => console.log('error', data),
+  });
+
   return {
     DOM: vdom$,
     SpeechSynthesis: synth$,
--- src/speech_synthesis.ts
+++ src/speech_synthesis.ts
@@ -1,21 +1,41 @@
-import xs from 'xstream'
+import xs, {Stream} from 'xstream'
+import fromEvent from 'xstream/extra/fromEvent'
 import {adapt} from '@cycle/run/lib/adapt'
 
 
+class UtteranceSource {
+  constructor(
+    private _utterance: SpeechSynthesisUtterance,
+  ) {}
+
+  events(eventType: string): Stream<Event> {
+    return adapt(fromEvent(this._utterance, eventType));
+  }
+}
+
 export function makeSpeechSynthesisDriver() {
   const synthesis: SpeechSynthesis = window.speechSynthesis;
+  const utterance: SpeechSynthesisUtterance = new SpeechSynthesisUtterance();
 
   return function speechSynthesisDriver(sink$) {
     sink$.addListener({
-      next: (utterance) => {
-        if (!utterance) {
+      next: (props) => {
+        // array values are SpeechSynthesisUtterance properties that are not
+        //   event handlers; see
+        //   https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisUtterance
+        if (!props) {
           synthesis.cancel();
         } else {
+          ['lang', 'pitch', 'rate', 'text', 'voice', 'volume'].map(prop => {
+            if (prop in props) {
+              utterance[prop] = props[prop];
+            }
+          });
           synthesis.speak(utterance);
         }
       }
     });
 
-    return adapt(sink$);
+    return new UtteranceSource(utterance);
   }
 }
