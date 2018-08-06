a8e9aefde3228d932bd76e89fcbe314e21a1013f
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-08-06 12:12:55 -0700

    Update demo to actually play a sound

--- examples/audio_player_action/src/index.tsx
+++ examples/audio_player_action/src/index.tsx
@@ -8,8 +8,17 @@ import {makeAudioPlayerDriver} from '@cycle-robot-drivers/sound'
 function main(sources) {
   const vdom$ = xs.of((<div>Cycle.js AudioPlayerAction component demo</div>));
 
+  const audio$ = xs.of({src: require("../public/snd/IWohoo1.ogg")});
+
+  ['abort', 'error', 'ended', 'loadeddata'].map((key) => {
+    sources.AudioPlayer[key].addListener({
+      next: data => console.warn('ended', data),
+    });
+  })
+
   return {
     DOM: vdom$,
+    AudioPlayer: audio$,
   };
 }
 
