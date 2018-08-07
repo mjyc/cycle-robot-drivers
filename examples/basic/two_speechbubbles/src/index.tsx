d5ad5717b636eb63d59b5da6cd1e129d6f1ec875
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-08-07 10:55:29 -0700

    WIP: Update inputs for TwoSpeechbubbles component

--- examples/basic/two_speechbubbles/src/index.tsx
+++ examples/basic/two_speechbubbles/src/index.tsx
@@ -11,38 +11,38 @@ function main(sources) {
   const sbub$ = xs.create();
   window.onload = () => {
     setTimeout(() => {
-      sbub$.shamefullySendNext({type: 'message', value: 'Hello'});
+      sbub$.shamefullySendNext({type: 'DISPLAY_MESSAGE', value: 'Hello'});
     }, 1);
     // test overwriting the current goal
     setTimeout(() => {
-      sbub$.shamefullySendNext({type: 'message', value: 'World!'});
+      sbub$.shamefullySendNext({type: 'ASK_QUESTION', value: ['Hello', ['Hello', 'World!']]});
     }, 100);
-    // test canceling an active goal
-    setTimeout(() => {sbub$.shamefullySendNext(null);}, 200);
-    // test calling cancel on done; cancel must do nothing
-    setTimeout(() => {
-      sbub$.shamefullySendNext({type: 'choices', value: ['Hello', 'World!']});
-    }, 500);
-    // you must click a button here
-    setTimeout(() => {sbub$.shamefullySendNext(null);}, 2000);
+    // // test canceling an active goal
+    // setTimeout(() => {sbub$.shamefullySendNext(null);}, 200);
+    // // test calling cancel on done; cancel must do nothing
+    // setTimeout(() => {
+    //   sbub$.shamefullySendNext({type: 'choices', value: ['Hello', 'World!']});
+    // }, 500);
+    // // you must click a button here
+    // setTimeout(() => {sbub$.shamefullySendNext(null);}, 2000);
   };
 
-  const speechbubbleAction = TwoSpeechbubbles({
+  const speechbubbles = TwoSpeechbubbles({
     goal: sbub$,
     DOM: sources.DOM,
   });
 
-  speechbubbleAction.status.addListener({
+  speechbubbles.status.addListener({
     next: data => console.warn('status', data),
   });
-  speechbubbleAction.result.addListener({
+  speechbubbles.result.addListener({
     next: data => console.warn('result', data),
   });
 
-  const vdom$ = speechbubbleAction.DOM.map((speechbubble) => {
+  const vdom$ = speechbubbles.DOM.map((sdom) => {
     return (
       <div>
-        {speechbubble}
+        {sdom}
       </div>
     );
   });
--- face/src/TwoSpeechbubbles.tsx
+++ face/src/TwoSpeechbubbles.tsx
@@ -2,24 +2,52 @@ import Snabbdom from 'snabbdom-pragma';
 import xs from 'xstream'
 import {adapt} from '@cycle/run/lib/adapt'
 import isolate from '@cycle/isolate';
+
+import {initGoal, generateGoalID} from '@cycle-robot-drivers/action'
 import {IsolatedSpeechbubbleAction} from './SpeechbubbleAction'
 
 export function TwoSpeechbubbles(sources) {
-  const robot = IsolatedSpeechbubbleAction(sources);
-  const human = IsolatedSpeechbubbleAction(sources);
+
+  // define a type for goal
+  const firstSources = {
+    ...sources,
+    goal: sources.goal.map(goal => {
+      switch (goal.type) {
+        case 'DISPLAY_MESSAGE':
+          return {type: 'message', value: goal.value};
+        case 'ASK_QUESTION':
+          return {type: 'message', value: goal.value[0]};
+      }
+    }),
+  };
+
+  const secondSource = {
+    ...sources,
+    goal: sources.goal.map(goal => {
+      switch (goal.type) {
+        case 'DISPLAY_MESSAGE':
+          return null;
+        case 'ASK_QUESTION':
+          return {type: 'choices', value: goal.value[1]};
+      }
+    }),
+  };
+
+  const firstSink = IsolatedSpeechbubbleAction(firstSources);
+  const secondSink = IsolatedSpeechbubbleAction(secondSource);
 
   return {
-    DOM: adapt(xs.combine(robot.DOM, human.DOM).map(([rdom, hdom]) => (
+    DOM: adapt(xs.combine(firstSink.DOM, secondSink.DOM).map(([fdom, sdom]) => (
       <div>
         <div>
-          <span>Robot:</span> {rdom}
+          <span>Robot:</span> {fdom}
         </div>
         <div>
-          <span>Human:</span> {hdom}
+          <span>Human:</span> {sdom}
         </div>
       </div>
     ))),
-    status: adapt(robot.status),
-    result: adapt(robot.result),
+    status: adapt(firstSink.status),
+    result: adapt(firstSink.result),
   };
 }
