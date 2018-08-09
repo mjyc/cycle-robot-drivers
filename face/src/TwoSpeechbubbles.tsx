f41f6a5a9ff7ea7ffa0bab93466fc07d5d0a9b2d
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-08-08 20:27:52 -0700

    WIP: Send goal and cancel

--- face/src/TwoSpeechbubbles.tsx
+++ face/src/TwoSpeechbubbles.tsx
@@ -4,97 +4,166 @@ import pairwise from 'xstream/extra/pairwise'
 import {adapt} from '@cycle/run/lib/adapt'
 import isolate from '@cycle/isolate';
 
-import {Status, initGoal, generateGoalID} from '@cycle-robot-drivers/action'
+import {
+  GoalID, Goal, Status, Result, initGoal, generateGoalID,
+} from '@cycle-robot-drivers/action'
 import {IsolatedSpeechbubbleAction} from './SpeechbubbleAction'
 
 export function TwoSpeechbubbles(sources) {
-  // Process incoming streams
-  const goals$ = sources.goal.map(goal => {
-    if (!goal) {
-      return [null, null, null];
-    }
-    const goal_id = generateGoalID();
-    switch (goal.type) {
-      case 'SET_MESSAGE':
-        return [{
-          goal_id,
-          goal: {
-            type: 'MESSAGE',
-            value: goal.value,
-          },
-        }, null, goal.type];
-      case 'ASK_QUESTION':
-        return [{
-          goal_id,
-          goal: {
-            type: 'MESSAGE',
-            value: goal.value[0],
-          },
-        }, {
-          goal_id,
-          goal: {
-            type: 'CHOICE',
-            value: goal.value[1],
-          },
-        }, goal.type];
-    }
+  const firstGoal$ = xs.create();
+  const secondGoal$ = xs.create();
+
+  const first = IsolatedSpeechbubbleAction({
+    DOM: sources.DOM,
+    goal: firstGoal$,
+  });
+  const second = IsolatedSpeechbubbleAction({
+    DOM: sources.DOM,
+    goal: secondGoal$,
   });
 
-  // Create sub-components
-  const firstSources = {
-    ...sources,
-    goal: goals$.map(goal => {
-      return goal[0];
-    }),
-  };
-  const secondSource = {
-    ...sources,
-    goal: goals$.map(goal => goal[1]),
-  };
-  const firstSink = IsolatedSpeechbubbleAction(firstSources);
-  const secondSink = IsolatedSpeechbubbleAction(secondSource);
+  // Process incoming streams
+  type Action = {
+    type: string,
+    value: any,
+  }
 
-  // Prepare outgoing streams
-  const result$ = xs.merge(
-    xs.combine(goals$, firstSink.status)
-      .map(([goals, status]) => {
-        if (goals[2] === 'SET_MESSAGE'
-            && (goals[0] as any).goal_id.id === (status as any).goal_id.id
-            && (status as any).status === Status.ACTIVE) {
+  const action$ = xs.merge(
+    sources.goal.map(goal => {
+      if (!goal) {
+        return {
+          type: 'CANCEL',
+          value: null,
+        };
+      } else {
+        return {
+          type: 'GOAL',
+          value: (goal as any).goal_id ? goal : initGoal(goal),
+        }
+      }
+    }),
+    first.result.map(result => ({type: 'FIRST_RESULT', value: result})),
+    second.result.map(result => ({type: 'SECOND_RESULT', value: result})),
+  );
+
+  // Update the state
+  type State = {
+    goal_id: GoalID,
+    goal: any,
+    status: Status,
+    result: any,
+  };
+
+  const initialState: State = {
+    goal: null,
+    goal_id: generateGoalID(),
+    status: Status.SUCCEEDED,
+    result: null,
+  };
+
+  const state$ = action$.fold((state: State, action: Action) => {
+    console.debug('state', state, 'action', action);
+    if (state.status === Status.SUCCEEDED
+        || state.status === Status.PREEMPTED
+        || state.status === Status.ABORTED) {
+      if (action.type === 'GOAL') {
+        const goal = (action.value as Goal);
+        return {
+          ...state,
+          goal_id: goal.goal_id,
+          goal: goal.goal,
+          status: Status.ACTIVE,
+          result: null,
+        };
+      } else if (action.type === 'CANCEL') {
+        console.debug('Ignore cancel in done states');
+        return state;
+      }
+    } else if (state.status === Status.ACTIVE) {
+      if (action.type === 'FIRST_RESULT') {
+        if (state.goal.type === 'SET_MESSAGE') {
           return {
-            status,
-            result: true,
-          }
+            ...state,
+            status: (action.value as Result),
+          };
         }
-      }),
-    xs.combine(goals$.compose(pairwise), secondSink.result)
-      .map(([pair, result]) => {
-        const [prevGoals, curGoals] = (pair as [any, any]);
-        const curGoalType = curGoals[2];
-        if ((!curGoalType  // cancel (null) case
-             && (prevGoals[1] as any).goal_id.id === (result as any).status.goal_id.id
-             && (result as any).status.status === Status.PREEMPTED)
-            || (curGoalType === 'ASK_QUESTION'
-                && (curGoals[1] as any).goal_id.id === (result as any).status.goal_id.id
-                && ((result as any).status.status === Status.SUCCEEDED
-                    || (result as any).status.status === Status.PREEMPTED
-                    || (result as any).status.status === Status.ABORTED))) {
-          return result;
+      } else if (action.type === 'CANCEL') {
+        return {
+          ...state,
+          goal: null,
+          status: Status.PREEMPTED,
         }
-      }),
-  ).filter(result => !!result);
+      }
+    }
+    console.warn(
+      `Unhandled state.status ${state.status} action.type ${action.type}`
+    );
+    return state;
+  }, initialState);
+
+  const stateStatusChanged$ = state$
+    .compose(pairwise)
+    .filter(([prevState, curState]) => (
+      curState.status !== prevState.status
+      || curState.goal_id.id !== prevState.goal_id.id
+     ))
+    .map(([prevState, curState]) => curState);
+
+  firstGoal$.imitate(stateStatusChanged$.map(state => {
+    if (!state.goal) {
+      return null;
+    }
+    switch (state.goal.type) {
+      case 'SET_MESSAGE':
+        return {
+          goal_id: state.goal_id,
+          goal: {
+            type: 'MESSAGE',
+            value: state.goal.value,
+          },
+        };
+      case 'ASK_QUESTION':
+        return {
+          goal_id: state.goal_id,
+          goal: {
+            type: 'MESSAGE',
+            value: state.goal.value[0],
+          },
+        };
+    };
+  }));
+  secondGoal$.imitate(stateStatusChanged$.map(state => {
+    if (!state.goal) {
+      return null;
+    }
+    switch (state.goal.type) {
+      case 'SET_MESSAGE':
+        return null;
+      case 'ASK_QUESTION':
+        return {
+          goal_id: state.goal_id,
+          goal: {
+            type: 'CHOICE',
+            value: state.goal.value[1],
+          },
+        };
+    };
+  }));
+
+  stateStatusChanged$.addListener({next: data => console.log(data.goal)});
+  // stateStatusChanged$.addListener({next: data => console.log(data)});
 
   return {
-    DOM: adapt(xs.combine(firstSink.DOM, secondSink.DOM).map(([fdom, sdom]) => (
+    DOM: adapt(xs.combine(first.DOM, second.DOM).map(([fdom, sdom]) => (
       <div>
         <div>
-          <span>Robot:</span> {fdom}
+          <span>Robot:</span> <span>{fdom}</span>
         </div>
         <div>
-          <span>Human:</span> {sdom}
+          <span>Human:</span> <span>{sdom}</span>
         </div>
       </div>
     ))),
-    result: adapt(result$),
+    result: adapt(xs.create()),
   };
 }
