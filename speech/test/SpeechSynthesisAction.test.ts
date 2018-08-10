2d389046e5aae7bd14e7e29dfc04d4c29b1bd942
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-08-10 14:18:01 -0700

    Update test to cover output "value"

--- speech/test/SpeechSynthesisAction.test.ts
+++ speech/test/SpeechSynthesisAction.test.ts
@@ -4,46 +4,55 @@ import {
 } from '@cycle-robot-drivers/action'
 import {SpeechSynthesisAction} from '../src/SpeechSynthesisAction';
 
+// hide debug outputs
+console.debug = jest.fn();
+
 describe('SpeechSynthesisAction', () => {
   it('emits status and results', (done) => {
     const Time = mockTimeSource();
 
-    const goalT$ =           Time.diagram(`---x-------|`);
+    // Create test input streams with time
+    const goalStr$ =           Time.diagram(`-x---|`);
     const events = {
-      start:                 Time.diagram(`----x------|`),
-      end:                   Time.diagram(`-----x-----|`),
-      error:                 Time.diagram(`-----------|`),
+      start:                   Time.diagram(`--x--|`),
+      end:                     Time.diagram(`---x-|`),
+      error:                   Time.diagram(`-----|`),
     }
-    const expectedStatusT$ = Time.diagram(`---pas-----|`);
-    const expectedResultT$ = Time.diagram(`-----x-----|`);
+    const expectedValueStr$ =  Time.diagram(`-y---|`);
+    const expectedStatusStr$ = Time.diagram(`-pas-|`);
+    const expectedResultStr$ = Time.diagram(`---y-|`);
 
+    // update strings to proper inputs
+    const goal = {text: 'Hello world!'};
     const goal_id = generateGoalID();
-    const goal$ = goalT$.map(trigger => {
-      return {
-        goal_id,
-        goal: {text: 'Hello world!'}
-      }
+    const toStatus = ((goal_id) => {
+      return (str) => {
+        switch (str) {
+          case 'p':
+            return {
+              goal_id,
+              status: Status.PENDING,
+            };
+          case 'a':
+            return {
+              goal_id,
+              status: Status.ACTIVE,
+            };
+          case 's':
+            return {
+              goal_id,
+              status: Status.SUCCEEDED,
+            };
+        }
+      };
+    })(goal_id);
+    const goal$ = goalStr$.mapTo({
+      goal_id,
+      goal,
     });
-    const expectedStatus$ = expectedStatusT$.map(trigger => {
-      switch (trigger) {
-        case 'p':
-          return {
-            goal_id,
-            status: Status.PENDING
-          }
-        case 'a':
-          return {
-            goal_id,
-            status: Status.ACTIVE
-          }
-        case 's':
-          return {
-            goal_id,
-            status: Status.SUCCEEDED
-          }
-      }
-    });
-    const expectedResult$ = expectedResultT$.mapTo({
+    const expectedValue$ = expectedValueStr$.mapTo(goal);
+    const expectedStatus$ = expectedStatusStr$.map(str => toStatus(str));
+    const expectedResult$ = expectedResultStr$.mapTo({
       status: {
         goal_id,
         status: Status.SUCCEEDED
@@ -61,10 +70,7 @@ describe('SpeechSynthesisAction', () => {
       }
     });
 
-    speechSynthesisAction.value.addListener({
-      next: data => console.warn('value', data),
-    });
-
+    Time.assertEqual(speechSynthesisAction.value, expectedValue$);
     Time.assertEqual(speechSynthesisAction.status, expectedStatus$);
     Time.assertEqual(speechSynthesisAction.result, expectedResult$);
 
