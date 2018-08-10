b5d235fdf0cb8b2929073bcb97f9e02367e374ff
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-08-10 11:19:32 -0700

    Improve skeleton test

--- speech/test/SpeechSynthesisAction.test.ts
+++ speech/test/SpeechSynthesisAction.test.ts
@@ -1,9 +1,51 @@
-// import {SpeechSynthesisAction} from '../src/SpeechSynthesisAction'
-import isolate from '@cycle/isolate';
+import {mockTimeSource} from '@cycle/time';
+import {SpeechSynthesisAction} from '../src/SpeechSynthesisAction';
 
-describe('hello', () => {
-  test('world', () => {
-    console.log(isolate);
-    expect(true).toBe(true);
+describe('SpeechSynthesisAction', () => {
+  it('xxx', (done) => {
+    const Time = mockTimeSource();
+
+    // const addClick$      = Time.diagram(`---x--x-------x--x--|`);
+    // const subtractClick$ = Time.diagram(`---------x----------|`);
+    // const expectedCount$ = Time.diagram(`0--1--2--1----2--3--|`);
+
+    // const DOM = mockDOMSource({
+    //   '.add': {
+    //     click: addClick$
+    //   },
+
+    //   '.subtract': {
+    //     click: subtractClick$
+    //   },
+    // });
+
+    const sendGoal$ =   Time.diagram(`---x---|`);
+    const events = {
+      start:            Time.diagram(`----x--|`),
+      end:              Time.diagram(`-----x-|`),
+      error:              Time.diagram(`-----x-|`),
+    }
+    sendGoal$.mapTo({text: 'Hello'});
+
+
+
+    const speechSynthesisAction = SpeechSynthesisAction({
+      goal: sendGoal$,
+      SpeechSynthesis: {
+        events: (eventName) => {
+          return events[eventName];
+        }
+      }
+    });
+
+    speechSynthesisAction.result.addListener({
+      next: data => console.warn('result', data),
+    });
+
+    // const count$ = counter.DOM.map(vtree => select('.count', vtree)[0].text);
+
+    // Time.assertEqual(count$, expectedCount$);
+
+    Time.run(done);
   });
 });
