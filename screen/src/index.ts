420b9094d1203ad5f954e1653af78062da96e6c8
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-08-16 17:52:01 -0700

    Export speechbubble types

--- screen/src/index.ts
+++ screen/src/index.ts
@@ -1,9 +1,11 @@
 export {makeFacialExpressionActionDriver} from './facial_expression_action'
 export {
+  SpeechbubbleType
   SpeechbubbleAction,
   IsolatedSpeechbubbleAction,
 } from './SpeechbubbleAction'
 export {
+  TwoSpeechbubblesType,
   TwoSpeechbubblesAction,
   IsolatedTwoSpeechbubblesAction,
 } from './TwoSpeechbubblesAction'
