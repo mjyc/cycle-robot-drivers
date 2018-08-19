1bbe6c784948de4ee705949a7818c491a16bef83
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-08-19 15:47:11 -0700

    Revive demo and index

--- examples/basic/facial_expression_action/src/index.tsx
+++ examples/basic/facial_expression_action/src/index.tsx
@@ -1,51 +1,57 @@
-// import Snabbdom from 'snabbdom-pragma';
-// import xs from 'xstream';
-// import {run} from '@cycle/run';
-// import {makeDOMDriver} from '@cycle/dom';
-// import {
-//   makeFacialExpressionActionDriver,
-// } from '@cycle-robot-drivers/screen'
+import Snabbdom from 'snabbdom-pragma';
+import xs from 'xstream';
+import {run} from '@cycle/run';
+import {makeDOMDriver} from '@cycle/dom';
+import {
+  makeTabletFaceDriver,
+  IsolatedFacialExpressionAction as FacialExpressionAction,
+} from '@cycle-robot-drivers/screen'
 
 
-// function main(sources) {
-//   const vdom$ = sources.FacialExpressionAction.DOM.map((face) => {
-//     return (
-//       <div>
-//         <p>FacialExpressionAction driver test</p>
-//         {face}
-//       </div>
-//     );
-//   });
+function main(sources) {
+  const vdom$ = sources.TabletFace.DOM.map((face) => {
+    return (
+      <div>
+        <p>FacialExpressionAction component test</p>
+        {face}
+      </div>
+    );
+  });
 
-//   const fexp$ = xs.create();
-//   window.onload = () => {
-//     setTimeout(() => fexp$.shamefullySendNext({type: 'happy'}), 1);
-//     // test overwriting the current goal
-//     setTimeout(() => fexp$.shamefullySendNext({type: 'sad'}), 100);
-//     // test canceling an active goal
-//     setTimeout(() => {fexp$.shamefullySendNext(null);}, 500);
-//     // test calling cancel on done; cancel must do nothing
-//     setTimeout(() => fexp$.shamefullySendNext({type: 'confused'}), 1000);
-//     setTimeout(() => fexp$.shamefullySendNext(null), 2500);
-//   };
+  const fexp$ = xs.create();
+  window.onload = () => {
+    setTimeout(() => fexp$.shamefullySendNext({type: 'happy'}), 1);
+    // test overwriting the current goal
+    setTimeout(() => fexp$.shamefullySendNext({type: 'sad'}), 100);
+    // test canceling an active goal
+    setTimeout(() => {fexp$.shamefullySendNext(null);}, 500);
+    // test calling cancel on done; cancel must do nothing
+    setTimeout(() => fexp$.shamefullySendNext({type: 'confused'}), 1000);
+    setTimeout(() => fexp$.shamefullySendNext(null), 2500);
+  };
 
-//   sources.FacialExpressionAction.result.addListener({
-//     next: data => console.warn('result', data),
-//   });
-//   sources.FacialExpressionAction.status.addListener({
-//     next: data => console.warn('status', data),
-//   });
-//   sources.FacialExpressionAction.ended.addListener({
-//     next: data => console.warn('ended', data),
-//   });
+  const facialExpressionAction = FacialExpressionAction({
+    goal: fexp$,
+    SpeechSynthesis: sources.SpeechSynthesis,
+  });
 
-//   return {
-//     DOM: vdom$,
-//     FacialExpressionAction: fexp$,
-//   };
-// }
+  facialExpressionAction.result.addListener({
+    next: data => console.warn('result', data),
+  });
+  facialExpressionAction.status.addListener({
+    next: data => console.warn('status', data),
+  });
+  // facialExpressionAction.ended.addListener({
+  //   next: data => console.warn('ended', data),
+  // });
 
-// run(main, {
-//   DOM: makeDOMDriver('#app'),
-//   FacialExpressionAction: makeFacialExpressionActionDriver(),
-// });
+  return {
+    DOM: vdom$,
+    TabletFace: facialExpressionAction.value,
+  };
+}
+
+run(main, {
+  DOM: makeDOMDriver('#app'),
+  TabletFace: makeTabletFaceDriver({faceHeight: '600px'}),
+});
