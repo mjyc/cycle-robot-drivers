d55952286750938480333c9a73b5f462134568b7
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-09-12 12:48:12 -0700

    Add "cancels a running goal on cancel" test

--- speech/test/SpeechRecognitionAction.test.ts
+++ speech/test/SpeechRecognitionAction.test.ts
@@ -81,4 +81,51 @@ describe('SpeechRecognitionAction', () => {
 
     Time.run(done);
   });
+
+  it('cancels a running goal on cancel"', (done) => {
+    const Time = mockTimeSource();
+
+    // Create test input streams with time
+    const goalMark$ =           Time.diagram(`-0-1---|`);
+    const events = {
+      start:                    Time.diagram(`--x----|`),
+      end:                      Time.diagram(`-----x-|`),
+      result:                   Time.diagram(`-------|`),
+      error:                    Time.diagram(`----x--|`),
+    }
+    const expectedOutputMark$ = Time.diagram(`-0-1---|`);
+    const expectedResultMark$ = Time.diagram(`-----p-|`);
+
+    // update strings to proper inputs
+    const transcript = 'Jello there?';
+    const goal = {};
+    const goal_id = generateGoalID();
+    const goals = [{goal, goal_id}, null];
+    const goal$ = goalMark$.map(i => goals[i]);
+
+    // Create the action to test
+    const speechRecognitionAction = SpeechRecognitionAction({
+      goal: goal$,
+      SpeechRecognition: {
+        events: (eventName) => {
+          return events[eventName];
+        }
+      }
+    });
+
+    // Prepare expected values
+    const values = [goal, null];
+    const toStatus = createToStatus(goal_id);
+    const expectedOutput$ = expectedOutputMark$.map(i => values[i]);
+    const expectedResult$ = expectedResultMark$.map(str => ({
+      status: toStatus(str),
+      result: null,
+    }));
+
+    // Run test
+    Time.assertEqual(speechRecognitionAction.output, expectedOutput$);
+    Time.assertEqual(speechRecognitionAction.result, expectedResult$);
+
+    Time.run(done);
+  });
 });
