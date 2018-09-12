0a103ca07c56b8af59b8f7d69edb7a73a88fb2ea
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-09-12 12:28:35 -0700

    Add happy path test for SpeechRecognitionAction

---
+++ speech/test/SpeechRecognitionAction.test.ts
@@ -0,0 +1,84 @@
+import xs from 'xstream'
+import {mockTimeSource} from '@cycle/time';
+import {
+  GoalID, GoalStatus, Status,
+  generateGoalID,
+} from '@cycle-robot-drivers/action'
+import {SpeechRecognitionAction} from '../src/SpeechRecognitionAction';
+
+
+console.debug = jest.fn();  // hide debug outputs
+
+function createToStatus(goal_id: GoalID) {
+  return function (str: string): GoalStatus {
+    switch (str) {
+      case 'a':
+        return {
+          goal_id,
+          status: Status.ACTIVE,
+        };
+      case 'p':
+        return {
+          goal_id,
+          status: Status.PREEMPTED,
+        };
+      case 's':
+        return {
+          goal_id,
+          status: Status.SUCCEEDED,
+        };
+    }
+  };
+};
+
+
+describe('SpeechRecognitionAction', () => {
+  it('walks through "happy path"', (done) => {
+    const Time = mockTimeSource();
+
+    // Create test input streams with time
+    const goalMark$ =           Time.diagram(`-x----|`);
+    const events = {
+      start:                    Time.diagram(`--x---|`),
+      end:                      Time.diagram(`----x-|`),
+      result:                   Time.diagram(`---x--|`),
+      error:                    Time.diagram(`------|`),
+    }
+    const expectedOutputMark$ = Time.diagram(`-x----|`);
+    const expectedResultMark$ = Time.diagram(`----s-|`);
+
+    // Create the action to test
+    const goal = {};
+    const goal_id = generateGoalID();
+    const goal$ = goalMark$.mapTo({
+      goal_id,
+      goal,
+    });
+    const transcript = 'Hello there';
+    events.result = events.result.map(r => ({
+      results: [[{transcript}]],
+    }));
+    const speechRecognitionAction = SpeechRecognitionAction({
+      goal: goal$,
+      SpeechRecognition: {
+        events: (eventName) => {
+          return events[eventName];
+        }
+      }
+    });
+
+    // Prepare expected values
+    const toStatus = createToStatus(goal_id);
+    const expectedOutput$ = expectedOutputMark$.mapTo(goal);
+    const expectedResult$ = expectedResultMark$.map(str => ({
+      status: toStatus(str),
+      result: transcript,
+    }));
+
+    // Run test
+    Time.assertEqual(speechRecognitionAction.output, expectedOutput$);
+    Time.assertEqual(speechRecognitionAction.result, expectedResult$);
+
+    Time.run(done);
+  });
+});
