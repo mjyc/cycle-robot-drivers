c17b1fbcd1a93848937575bf15e07c6aa01d265d
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-09-15 09:35:01 -0700

    WIP

--- examples/tutorials/01_getting_started/src/index.tsx
+++ examples/tutorials/01_getting_started/src/index.tsx
@@ -3,11 +3,6 @@ import xs from 'xstream';
 import {run} from '@cycle/run';
 import {makeDOMDriver} from '@cycle/dom';
 import {
-  makeTabletFaceDriver,
-  makeFacialExpressionActionDriver,
-  makeTwoSpeechbubblesActionDriver,
-} from '@cycle-robot-drivers/screen';
-import {
   makeSpeechSynthesisActionDriver,
   makeSpeechRecognitionActionDriver,
 } from '@cycle-robot-drivers/speech';
@@ -32,22 +27,55 @@ function main(sources) {
   });
 
   return {
-    FacialExpressionAction: xs.periodic(2000).mapTo({type: 'happy'}),
+    // FacialExpressionAction: xs.periodic(2000).mapTo({type: 'happy'}),
     SpeechSynthesisAction: goal$,
     SpeechRecognitionAction: xs.of({}),
     // TabletFace: xs.periodic(2000).mapTo({type: 'EXPRESS', args: {type: 'happy'}}),
   };
 }
 
-const DOMDriver = makeDOMDriver('#app');
-const tabletFaceDriver = makeTabletFaceDriver({
-  styles: {faceHeight: '600px'},
-  DOMDriver,
-});
+// const DOMDriver = makeDOMDriver('#app');
+// const tabletFaceDriver = makeTabletFaceDriver({
+//   styles: {faceHeight: '600px'},
+//   DOMDriver,
+// });
+
+
+function powerup(main, connect) {
+  return (sources) => {
+    const sinks = main(sources);
+    Object.keys(sources.proxies).map(key => {
+      connect(sources.proxies[key], sinks.targets[key]);
+    });
+    const {targets, ...sinksNoTargets} = sinks;
+    return sinksNoTargets;
+  };
+}
+
+// function main_temp(sources) {
+//   sources.proxies = {
+//     firstGoal: xs.create(),
+//     secondGoal: xs.create(),
+//   };
+
+
+
+//   return {
+//     FacialExpressionAction
+//     SpeechSynthesisAction
+//     SpeechRecognitionAction
+//     TabletFace
+//     TwoSpeechbubblesActionDriver
+
+//     targets: {
+//       firstGoal: firstGoal$,
+//       secondGoal: secondGoal$
+//     },
+//   }
+// }
+
 run(main, {
-  FacialExpressionAction: makeFacialExpressionActionDriver({tabletFaceDriver}),
+  // FacialExpressionAction: makeFacialExpressionActionDriver({tabletFaceDriver}),
   SpeechSynthesisAction: makeSpeechSynthesisActionDriver(),
   SpeechRecognitionAction: makeSpeechRecognitionActionDriver(),
-  TabletFace: tabletFaceDriver,
-  TwoSpeechbubblesActionDriver: makeTwoSpeechbubblesActionDriver({DOMDriver})
 });
