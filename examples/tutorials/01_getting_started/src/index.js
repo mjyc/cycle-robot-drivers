71218127ba0d1ea47ded49bc5fe6c235931dfa74
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-09-22 12:39:23 -0700

    Update getting started example

--- examples/tutorials/01_getting_started/src/index.js
+++ examples/tutorials/01_getting_started/src/index.js
@@ -2,13 +2,16 @@ import {makeDOMDriver} from '@cycle/dom';
 import {runRobotProgram} from '@cycle-robot-drivers/run';
 import xs from 'xstream';
 
-
 function main(sources) {
-  const hello$ = sources.TabletFace.load.map('Hello!');
+  const hello$ = sources.TabletFace.load.mapTo('Hello!');
+  const nice$ = sources.SpeechSynthesisAction.result
+    .take(1)
+    .mapTo('Nice to meet you!');
+  const greet$ = xs.of(hello$, nice$);
     
   return {
-    TwoSpeechbubblesAction: hello$,
-    SpeechSynthesisAction: hello$,
+    TwoSpeechbubblesAction: greet$,
+    SpeechSynthesisAction: greet$,
   }
 }
 
