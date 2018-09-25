64b522e275686e0446940ebdc445b62d33d44b30
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-09-25 15:50:07 -0700

    Update 2nd tutorial

--- examples/tutorials/02_action_drivers/index.js
+++ examples/tutorials/02_action_drivers/index.js
@@ -6,14 +6,19 @@ function main(sources) {
   const message$ = xs.merge(
     xs.of('Hello').compose(delay(1000)),
     // xs.of(null).compose(delay(1500)),
-    // xs.of('world').compose(delay(1500)),
+    // xs.of('World').compose(delay(1500)),
   );
 
+  sources.SpeechSynthesisAction.output.addListener({
+    next: output => console.log('output', output),
+  });
   sources.SpeechSynthesisAction.result.addListener({
-    next: result => console.log(result),
+    next: result => console.log('result', result),
   });
   
   return {
+    // SpeechSynthesis: sources.SpeechSynthesisAction.output.drop(1),
+    // SpeechSynthesis: xs.of('What?!').compose(delay(1000)),
     SpeechSynthesisAction: message$,
   };
 }
