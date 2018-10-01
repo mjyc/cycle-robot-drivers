e885881090f2885d64133a4181082a7b5bf421bc
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-10-01 09:13:06 -0700

    Prepare tutorial1 to 2nd phase

--- examples/tutorials/01_personality_quiz/index.js
+++ examples/tutorials/01_personality_quiz/index.js
@@ -1,6 +1,23 @@
 import xs from 'xstream';
 import {runRobotProgram} from '@cycle-robot-drivers/run';
 
-function main(sources) { }
+function main(sources) {
+  const sinks = {
+    TabletFace: xs.periodic(1000).map(i => {
+      const position = {
+        x: i % 2 === 0 ? 0 : 1,  // horizontal left or right
+        y: 0.5,  // vertical center
+      };
+      return {
+        type: 'SET_STATE',
+        value: {
+          leftEye: position,
+          rightEye: position,
+        }
+      };
+    })
+  };
+  return sinks;
+}
 
 runRobotProgram(main);
