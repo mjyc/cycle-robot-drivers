8c7b92fd89cb3fb3e9e85c215c66155ffda9cade
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-10-01 08:51:31 -0700

    Prepare tutorial1 to 1st phase

--- examples/tutorials/01_personality_quiz/index.js
+++ examples/tutorials/01_personality_quiz/index.js
@@ -1,18 +1,6 @@
 import xs from 'xstream';
 import {runRobotProgram} from '@cycle-robot-drivers/run';
 
-function main(sources) {
-  const hello$ = sources.TabletFace.load.mapTo('Hello!');
-  const nice$ = sources.SpeechSynthesisAction.result
-    .take(1)
-    .mapTo('Nice to meet you!');
-  const greet$ = xs.merge(hello$, nice$);
-  
-  const sink = {
-    TwoSpeechbubblesAction: greet$,
-    SpeechSynthesisAction: greet$,
-  };
-  return sink;
-}
+function main(sources) { }
 
 runRobotProgram(main);
