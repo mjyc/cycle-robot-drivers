7eab85e8c175785ab86bf50b35d9f95e0dd065c2
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-10-03 16:48:07 -0700

    WIP: Update to FSM

--- examples/simple/personality_quiz/index.js
+++ examples/simple/personality_quiz/index.js
@@ -1,6 +1,7 @@
 import xs from 'xstream';
 import sampleCombine from 'xstream/extra/sampleCombine';
 import {runRobotProgram} from '@cycle-robot-drivers/run';
+import { reduceEachLeadingCommentRange, isNonNullExpression } from 'typescript';
 
 const Question = {
   CAREER: 'Is it important that you reach your full career potential?',
@@ -51,56 +52,88 @@ const transitionTable = {
   }
 };
 
-function main(sources) {
-  sources.SpeechRecognitionAction.result.addListener({
-    next: (result) => console.log('result', result)
-  });
-  const lastQuestion$ = xs.create();
-  const question$ = xs.merge(
-    sources.TabletFace.load.mapTo(Question.CAREER),
-    sources.SpeechRecognitionAction.result.filter(result =>
-      result.status.status === 'SUCCEEDED'  // must succeed
-      && (result.result === 'yes' || result.result === 'no') // only yes or no
-    ).map(result => result.result)
-    .startWith('')
-    .compose(sampleCombine(
-      lastQuestion$
-    )).map(([response, question]) => {
-      return transitionTable[question][response];
-    })
-  );
-  lastQuestion$.imitate(question$);
+const InputType = {
+  LOADED: 'LOADED',
+  RECEIVED_RESPONSE: 'RECEIVED_RESPONSE',
+};
 
-  const sinks = {
-    TabletFace: sources.PoseDetection.poses
-      .filter(poses =>
-        // must see one person
-        poses.length === 1
-        // must see the nose
-        && poses[0].keypoints.filter(kpt => kpt.part === 'nose').length === 1
-      ).map(poses => {
-        const nose = poses[0].keypoints.filter(kpt => kpt.part === 'nose')[0];
-        return {
-          x: nose.position.x / 640,  // max value of position.x is 640
-          y: nose.position.y / 480  // max value of position.y is 480
-        };
-      }).map(position => ({
-        type: 'SET_STATE',
-        value: {
-          leftEye: position,
-          rightEye: position
-        }
-      })),
-    SpeechSynthesisAction: question$,
-    SpeechRecognitionAction: xs.merge(
-      sources.SpeechSynthesisAction.result,
-      sources.SpeechRecognitionAction.result.filter(result =>
-        result.status.status !== 'SUCCEEDED'  // must succeed
-        || (result.result !== 'yes' && result.result !== 'no') // only yes or no
-      )
-    ).mapTo({})
-  };
-  return sinks;
+function input(
+  load$,
+  speechRecognitionActionResult$,
+) {
+  return xs.merge(
+    load.mapTo({type: InputType.LOADED, value: null}),
+    speechRecognitionActionResult$.filter(result =>
+      result.status.status === 'SUCCEEDED'
+      && (result.result === 'yes' || result.result === 'no')
+    ).map(result => ({
+      type: InputType.RECEIVED_RESPONSE,
+      value: result.result
+    })),
+    // SpeechRecognitionAction: xs.merge(
+    //   sources.SpeechSynthesisAction.result,
+    //   sources.SpeechRecognitionAction.result.filter(result =>
+    //     result.status.status !== 'SUCCEEDED'  // must succeed
+    //     || (result.result !== 'yes' && result.result !== 'no') // only yes or no
+    //   )
+    // ).mapTo({})
+  );
+}
+
+function main(sources) {
+  console.log('Hello world!');
+
+  const input$ = input(sources);
+
+  // sources.SpeechRecognitionAction.result.addListener({
+  //   next: (result) => console.log('result', result)
+  // });
+  // const lastQuestion$ = xs.create();
+  // const question$ = xs.merge(
+  //   sources.TabletFace.load.mapTo(Question.CAREER),
+  //   sources.SpeechRecognitionAction.result.filter(result =>
+  //     result.status.status === 'SUCCEEDED'  // must succeed
+  //     && (result.result === 'yes' || result.result === 'no') // only yes or no
+  //   ).map(result => result.result)
+  //   .startWith('')
+  //   .compose(sampleCombine(
+  //     lastQuestion$
+  //   )).map(([response, question]) => {
+  //     return transitionTable[question][response];
+  //   })
+  // );
+  // lastQuestion$.imitate(question$);
+
+  // const sinks = {
+  //   TabletFace: sources.PoseDetection.poses
+  //     .filter(poses =>
+  //       // must see one person
+  //       poses.length === 1
+  //       // must see the nose
+  //       && poses[0].keypoints.filter(kpt => kpt.part === 'nose').length === 1
+  //     ).map(poses => {
+  //       const nose = poses[0].keypoints.filter(kpt => kpt.part === 'nose')[0];
+  //       return {
+  //         x: nose.position.x / 640,  // max value of position.x is 640
+  //         y: nose.position.y / 480  // max value of position.y is 480
+  //       };
+  //     }).map(position => ({
+  //       type: 'SET_STATE',
+  //       value: {
+  //         leftEye: position,
+  //         rightEye: position
+  //       }
+  //     })),
+  //   SpeechSynthesisAction: question$,
+  //   SpeechRecognitionAction: xs.merge(
+  //     sources.SpeechSynthesisAction.result,
+  //     sources.SpeechRecognitionAction.result.filter(result =>
+  //       result.status.status !== 'SUCCEEDED'  // must succeed
+  //       || (result.result !== 'yes' && result.result !== 'no') // only yes or no
+  //     )
+  //   ).mapTo({})
+  // };
+  // return sinks;
 }
 
 runRobotProgram(main);
