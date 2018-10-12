86266e767972fb8330ed8f25d9aeea1b2a5bf824
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-10-11 18:17:50 -0700

    Update fsm tutorial

--- examples/tutorials/02_fsm/index.js
+++ examples/tutorials/02_fsm/index.js
@@ -1,4 +1,5 @@
 import xs from 'xstream';
+import delay from 'xstream/extra/delay'
 import {runRobotProgram} from '@cycle-robot-drivers/run';
 
 const State = {
@@ -8,7 +9,7 @@ const State = {
 };
 
 const InputType = {
-  GOAL: `GOAL`,
+  START: `START`,
   SAY_DONE: `SAY_DONE`,
   VALID_RESPONSE: `VALID_RESPONSE`,
   INVALID_RESPONSE: `INVALID_RESPONSE`,
@@ -55,16 +56,62 @@ function transition(state, variables, input) {  // a dummy transition function
  * }
  */
 
-function input() {  // a dummy input function
-  return xs.never();
+function input(
+  start$,
+  speechRecognitionActionResult$,
+  speechSynthesisActionResult$,
+  poses$,
+) {
+  return xs.merge(
+    start$.mapTo({type: InputType.START}),
+    speechRecognitionActionResult$
+      .filter(result =>
+        result.status.status === 'SUCCEEDED'
+        && (result.result === Response.YES || result.result === Response.NO)
+      ).map(result => ({
+        type: InputType.VALID_RESPONSE,
+        value: result.result,
+      })),
+    speechSynthesisActionResult$
+      .filter(result => result.status.status === 'SUCCEEDED')
+      .mapTo({type: InputType.SAY_DONE}),
+    speechRecognitionActionResult$
+      .filter(result =>
+        result.status.status !== 'SUCCEEDED'
+        || (result.result !== Response.YES && result.result !== Response.NO)
+      ).mapTo({type: InputType.INVALID_RESPONSE}),
+    poses$
+      .filter(poses =>
+        poses.length === 1
+        && poses[0].keypoints.filter(kpt => kpt.part === 'nose').length === 1
+      ).map(poses => {
+        const nose = poses[0].keypoints.filter(kpt => kpt.part === 'nose')[0];
+        return {
+          type: InputType.DETECTED_FACE,
+          value: {
+            x: nose.position.x / 640,  // max value of position.x is 640
+            y: nose.position.y / 480,  // max value of position.y is 480
+          },
+        };
+      }),
+  );
 }
 
-function output() {
-  return {};
+function output(machine$) {
+  return {
+    SpeechSynthesisAction: xs.of('Hello world!').compose(delay(1000)),
+    SpeechRecognitionAction: xs.of({}).compose(delay(1000)),
+    TabletFace: xs.never(),
+  };
 }
 
 function main(sources) { 
-  const input$ = xs.never();
+  const input$ = input(
+    sources.TabletFace.load,
+    sources.SpeechSynthesisAction.result,
+    sources.SpeechRecognitionAction.result,
+    sources.PoseDetection.poses,
+  );
 
   const defaultMachine = {
     state: State.PEND,
@@ -73,10 +120,13 @@ function main(sources) {
     },
     outputs: null,
   };
-
-  const machine$ = input$.fold((machine, input) => transition(
+  const machine$ = input$.debug().fold((machine, input) => transition(
     machine.state, machine.variables, input
   ), defaultMachine);
+
+  machine$.addListener({
+    next: (value) => console.log('machine', value),
+  });
 
   const sinks = output();
   return sinks;
