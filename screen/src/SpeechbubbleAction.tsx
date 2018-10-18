534383160cf49d21203cd88bc99f977b165c7206
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-10-17 22:19:01 -0700

    Turn SpeechbubbleAction into simple FSM

--- screen/src/SpeechbubbleAction.tsx
+++ screen/src/SpeechbubbleAction.tsx
@@ -1,177 +1,179 @@
 import Snabbdom from 'snabbdom-pragma';
 import xs from 'xstream';
-import {MemoryStream} from 'xstream';  // to return MemoryType in SpeechbubbleAction; for typescript 2.8.x
-import dropRepeats from 'xstream/extra/dropRepeats';
+import {Stream} from 'xstream';
 import {adapt} from '@cycle/run/lib/adapt';
 import isolate from '@cycle/isolate';
 import {
-  GoalID, Goal, GoalStatus, Status, Result,
-  generateGoalID, initGoal, isEqual,
+  GoalID, Goal, Status, Result, EventSource, initGoal,
 } from '@cycle-robot-drivers/action';
+
 
 export enum SpeechbubbleType {
   MESSAGE = 'MESSAGE',
   CHOICE = 'CHOICE',
 }
 
-export function SpeechbubbleAction(sources) {
-  // Create action stream
-  type Action = {
-    type: string,
-    value: Goal | string | boolean,
-  };
 
-  const goal$ = xs.fromObservable(
-    sources.goal
-  ).filter(goal => typeof goal !== 'undefined').map(goal => {
-    if (goal === null) {
-      return {
-        type: 'CANCEL',
-        value: null,
-      };
-    } else {
-      const value = !!(goal as any).goal_id ? goal as any: initGoal(goal);
-      return {
-        type: 'GOAL',
-        value: !value.goal.type ? {
-          goal_id: value.goal_id,
-          goal: {
-            type: typeof value.goal === 'string'
-              ? SpeechbubbleType.MESSAGE
-              : SpeechbubbleType.CHOICE,
-            value: value.goal,
-          },
-        } : value,
-      };
-    }
-  });
-  // IMPORTANT!! force creating the click stream
-  let click$ = sources.DOM.select('.choice').elements()
-    .map(b => sources.DOM.select('.choice').events('click', {
-      preventDefault: true
-    })).flatten();
-  click$ = xs.fromObservable(click$).map((event: Event) => {
-    return {
-      type: 'CLICK',
-      value: (event.target as HTMLButtonElement).textContent,
-    };
-  });
-  const action$ = xs.merge(goal$, click$);
+enum State {
+  RUNNING = 'RUNNING',
+  DONE = 'DONE',
+}
 
-  // Create state stream
-  type State = {
-    goal_id: GoalID,
-    goal: any,
-    status: Status,
-    result: any,
-  };
+type Variables = {
+  goal_id: GoalID,
+  newGoal: Goal,
+};
 
-  const initialState: State = {
-    goal: null,
-    goal_id: generateGoalID(),
-    status: Status.SUCCEEDED,
+type Outputs = {
+  args: any,
+};
+
+type ReducerState = {
+  state: State,
+  variables: Variables,
+  outputs: Outputs,
+  result: Result,
+};
+
+type Reducer = (prev?: ReducerState) => ReducerState | undefined;
+
+enum InputType {
+  GOAL = 'GOAL',
+  CANCEL = 'CANCEL',
+}
+
+type Input = {
+  type: InputType,
+  value: Goal,
+};
+
+
+function input(goal$: Stream<any>, clickEvent$: Stream<any>) {
+  return xs.merge(
+    goal$.filter(goal => typeof goal !== 'undefined').map(goal => {
+      if (goal === null) {
+        return {
+          type: InputType.CANCEL,
+          value: null,  // means "cancel"
+        };
+      } else {
+        const value = !!(goal as any).goal_id ? goal : initGoal(goal);
+        return {
+          type: InputType.GOAL,
+          value: typeof value.goal === 'string'
+            ? {
+              goal_id: value.goal_id,
+              goal: {text: value.goal},
+            } : value,
+        };
+      }
+    }),
+  );
+}
+
+// function createTransition() {
+//   const transitionTable = {
+//     [State.PEND]: {
+//       [InputType.GOAL]: (variables) => ({
+//         state: State.TELL,
+//         variables,
+//         outputs: {
+//           SpeechSynthesisAction: {
+//             goal: story[variables.index],
+//           },
+//         },
+//       }),
+//     },
+//     [State.TELL]: {
+//       [InputType.TELL_DONE]: (variables) => {
+//         const storyIndex = variables.index + 1;
+//         if (storyIndex < story.length) {
+//           return {
+//             state: State.TELL,
+//             variables: {index: storyIndex},
+//             outputs: {
+//               SpeechSynthesisAction: {
+//                 goal: story[storyIndex],
+//               },
+//             },
+//           };
+//         } else {  // done
+//           return {
+//             state: State.PEND,
+//             variables: {index: storyIndex},
+//             outputs: {
+//               done: true,
+//             },
+//           };
+//         }
+//       }
+//     }
+//   };
+
+  return function(state, variables, input) {
+    return !transitionTable[state]
+      ? state
+      : !transitionTable[state][input.type]
+        ? state
+        : transitionTable[state][input.type](variables);
+  }
+}
+
+function transition(
+  prevState: State, prevVariables: Variables, input: Input
+): ReducerState {
+  return {
+    state: prevState,
+    variables: prevVariables,
+    outputs: null,
     result: null,
   };
+}
 
-  const state$ = action$.fold((state: State, action: Action): State => {
-    console.debug('state', state, 'action', action);
-    if (state.status === Status.SUCCEEDED
-        || state.status === Status.PREEMPTED
-        || state.status === Status.ABORTED) {
-      if (action.type === 'GOAL') {
-        const goal = (action.value as Goal);
-        return {
-          goal_id: goal.goal_id,
-          goal: goal.goal,
-          status: Status.ACTIVE,
-          result: null,
-        };
-      } else if (action.type === 'CANCEL') {
-        console.debug('Ignore CANCEL in DONE states');
-        return state;
-      }
-    } else if (state.status === Status.ACTIVE) {
-      if (action.type === 'GOAL') {
-        state$.shamefullySendNext({
-          ...state,
-          goal: null,
-          status: Status.PREEMPTED,
-        });
-        const goal = (action.value as Goal);
-        return {
-          goal_id: goal.goal_id,
-          goal: goal.goal,
-          status: Status.ACTIVE,
-          result: null,
-        };
-      } else if (action.type === 'CLICK') {
-        return {
-          ...state,
-          status: Status.SUCCEEDED,
-          result: (action.value as string),
-        };
-      } else if (action.type === 'CANCEL') {
-        return {
-          ...state,
-          goal: null,
-          status: Status.PREEMPTED,
-        };
+function transitionReducer(input$: Stream<Input>): Stream<Reducer> {
+  const initReducer$ = xs.of(
+    function initReducer(prev) {
+      return {
+        state: State.DONE,
+        variables: {
+          goal_id: null,
+          newGoal: null,
+        },
+        outputs: null,
+        result: null,
       }
     }
-    console.warn(
-      `Unhandled state.status ${state.status} action.type ${action.type}`
-    );
-    return state;
-  }, initialState);
+  );
 
-  // Prepare outgoing streams
-  const vdom$ = state$.map((state: State) => {
-    const innerDOM = (() => {
-      if (state.status === Status.ACTIVE) {
-        switch (state.goal.type) {
-          case SpeechbubbleType.MESSAGE:
-            return (<span>{state.goal.value}</span>);
-          case SpeechbubbleType.CHOICE:
-            return (
-              <span>{state.goal.value.map((text) => (
-                <button className="choice">{text}</button>
-              ))}</span>
-            );
-        }
-      } else {
-        return null;
-      }
-    })();
-    return innerDOM;
-  });
-  // IMPORTANT!! manually empty vdom$ stream to prevent the unexpected behavior
-  vdom$.addListener({next: vdom => {}});
+  const inputReducer$ = input$
+    .map(input => function inputReducer(prev) {
+      return transition(prev.state, prev.variables, input);
+    });
 
-  const stateStatusChanged$ = state$
-    .compose(dropRepeats(
-      (x, y) => (x.status === y.status && isEqual(x.goal_id, y.goal_id))));
+  return xs.merge(initReducer$, inputReducer$);
+}
 
-  const status$ = stateStatusChanged$
-    .map(state => ({
-      goal_id: state.goal_id,
-      status: state.status,
-    } as GoalStatus))
+export function SpeechbubbleAction(sources: {
+  goal: any,
+  SpeechSynthesis: EventSource,
+}): {
+  DOM: any,
+  result: any,
+} {
+  const input$ = input(
+    xs.fromObservable(sources.goal),
+    xs.never(),
+  );
 
-  const result$ = stateStatusChanged$
-    .filter(state => (state.status === Status.SUCCEEDED
-        || state.status === Status.PREEMPTED
-        || state.status === Status.ABORTED))
-    .map(state => ({
-      status: {
-        goal_id: state.goal_id,
-        status: state.status,
-      },
-      result: state.result,
-    } as Result));
+  const state$ = transitionReducer(input$)
+    .fold((state: ReducerState, reducer: Reducer) => reducer(state), null)
+    .drop(1);  // drop "null"
+  const outputs$ = state$.map(state => state.outputs)
+    .filter(outputs => !!outputs);
+  const result$ = state$.map(state => state.result).filter(result => !!result);
 
   return {
-    DOM: vdom$,
-    status: adapt(status$),
+    DOM: xs.of((<span>{'hello world'}</span>)),//adapt(outputs$.map(outputs => outputs.args)),
     result: adapt(result$),
   };
 }
