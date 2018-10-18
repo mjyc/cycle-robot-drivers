d7d896554861f85ad6059d78ee9a6fd2ac968e05
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-10-17 17:44:33 -0700

    Release 3rdparty/cycle-posenet-driver 0.0.16

--- 3rdparty/cycle-posenet-driver/src/pose_detection.tsx
+++ 3rdparty/cycle-posenet-driver/src/pose_detection.tsx
@@ -43,7 +43,7 @@ function setupGui(cameras, net, guiState) {
   input.add(guiState.input, 'outputStride', [8, 16, 32]);
   // Image scale factor: What to scale the image by before feeding it through
   // the network.
-  input.add(guiState.input, 'imageScaleFactor').min(0.2).max(1.0);
+  input.add(guiState.input, 'imageScaleFactor').min(0.1).max(1.0);
   input.open();
 
   // Pose confidence: the overall confidence in the estimation of a person's
