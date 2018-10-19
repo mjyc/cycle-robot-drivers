c6ab946c86fb9632eb5e7b5ead7fc6bc502f46bd
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-10-19 10:14:16 -0700

    Add dummy TwoSpeechbubblesAction and update screen demo

--- screen/src/TwoSpeechbubblesAction.tsx
+++ screen/src/TwoSpeechbubblesAction.tsx
@@ -1,16 +1,48 @@
-import Snabbdom from 'snabbdom-pragma';
 import xs from 'xstream';
-import dropRepeats from 'xstream/extra/dropRepeats';
+import {Stream} from 'xstream';
 import {adapt} from '@cycle/run/lib/adapt';
 import isolate from '@cycle/isolate';
 import {
-  GoalID, Goal, Status, GoalStatus, Result,
-  generateGoalID, initGoal, isEqual,
+  GoalID, Goal, Status, Result, initGoal,
 } from '@cycle-robot-drivers/action';
-import {
-  SpeechbubbleType,
-  IsolatedSpeechbubbleAction,
-} from './SpeechbubbleAction';
+
+
+enum State {
+  RUNNING = 'RUNNING',
+  DONE = 'DONE',
+}
+
+type Variables = {
+  goal_id: GoalID,
+  goal: Goal,
+  newGoal: Goal,
+};
+
+type Outputs = {
+  DOM: {
+    goal: any,
+  },
+  result: Result,
+};
+
+type Machine = {
+  state: State,
+  variables: Variables,
+  outputs: Outputs,
+};
+
+type Reducer = (machine?: Machine) => Machine | undefined;
+
+enum InputType {
+  GOAL = 'GOAL',
+  CANCEL = 'CANCEL',
+  CLICK = 'CLICK',
+}
+
+type Input = {
+  type: InputType,
+  value: Goal,
+};
 
 
 export enum TwoSpeechbubblesType {
@@ -18,240 +50,99 @@ export enum TwoSpeechbubblesType {
   ASK_QUESTION = 'ASK_QUESTION',
 }
 
-function main(sources) {
-  sources.proxies = {
-    firstGoal: xs.create(),
-    secondGoal: xs.create(),
-  };
-
-  const first = IsolatedSpeechbubbleAction({
-    DOM: sources.DOM,
-    goal: sources.proxies.firstGoal,
-  });
-  const second = IsolatedSpeechbubbleAction({
-    DOM: sources.DOM,
-    goal: sources.proxies.secondGoal,
-  });
-  // IMPORTANT!! empty the streams manually
-  first.DOM.addListener({next: d => {}});
-  second.DOM.addListener({next: d => {}})
-
-  // Process incoming streams
-  type Action = {
-    type: string,
-    value: any,
-  }
-
-  const goal$ = xs.fromObservable(
-    sources.goal
-  ).filter(goal => typeof goal !== 'undefined').map(goal => {
-    if (goal === null) {
-      return {
-        type: 'CANCEL',
-        value: null,
-      };
-    } else {
-      const value = !!(goal as any).goal_id ? goal as any: initGoal(goal);
-      return {
-        type: 'GOAL',
-        value: !value.goal.type ? {
-          goal_id: value.goal_id,
-          goal: {
-            type: typeof value.goal === 'string'
-              ? TwoSpeechbubblesType.SET_MESSAGE
-              : TwoSpeechbubblesType.ASK_QUESTION,
-            value: value.goal,
-          }
-        } : value,
-      };
-    }
-  });
-  const action$ = xs.merge(
-    goal$,
-    first.result.map(result => ({type: 'FIRST_RESULT', value: result})),
-    second.result.map(result => ({type: 'SECOND_RESULT', value: result})),
+function input(goal$: Stream<any>): Stream<Input> {
+  return xs.merge(
+    goal$.filter(goal => typeof goal !== 'undefined').map(goal => {
+      if (goal === null) {
+        return {
+          type: InputType.CANCEL,
+          value: null,  // means "cancel"
+        };
+      } else {
+        const value = !!(goal as any).goal_id ? goal : initGoal(goal);
+        return {
+          type: InputType.GOAL,
+          value: typeof value.goal === 'string'
+            ? {
+              goal_id: value.goal_id,
+              goal: {type: TwoSpeechbubblesType.SET_MESSAGE, value: value.goal},
+            } : Array.isArray(value.goal)
+              ? {
+                goal_id: value.goal_id,
+                goal: {type: TwoSpeechbubblesType.ASK_QUESTION, value: value.goal},
+              } : value.goal,  // {type: string, value: string | [string]}
+        };
+      }
+    }),
   );
+}
 
-  // Update the state
-  type State = {
-    goal_id: GoalID,
-    goal: any,
-    status: Status,
-    result: any,
-  };
-
-  const initialState: State = {
-    goal: null,
-    goal_id: generateGoalID(),
-    status: Status.SUCCEEDED,
-    result: null,
-  };
-
-  const state$ = action$.fold((state: State, action: Action) => {
-    console.debug('state', state, 'action', action);
-    if (state.status === Status.SUCCEEDED
-        || state.status === Status.PREEMPTED
-        || state.status === Status.ABORTED) {
-      if (action.type === 'GOAL') {
-        const goal = (action.value as Goal);
-        return {
-          goal_id: goal.goal_id,
-          goal: goal.goal,
-          status: Status.ACTIVE,
-          result: null,
-        };
-      } else if (action.type === 'CANCEL') {
-        console.debug('Ignore CANCEL in DONE states');
-        return state;
-      } else if (
-        action.type === 'FIRST_RESULT' || action.type === 'SECOND_RESULT'
-      ) {
-        console.debug('Ignore FIRST_RESULT and SECOND_RESULT in DONE states');
-        return state;
-      }
-    } else if (state.status === Status.ACTIVE) {
-      if (action.type === 'GOAL') {
-        state$.shamefullySendNext({
-          ...state,
-          goal: null,
-          status: Status.PREEMPTED,
-        });
-        const goal = (action.value as Goal);
-        return {
-          goal_id: goal.goal_id,
-          goal: goal.goal,
-          status: Status.ACTIVE,
-          result: null,
-        };
-      } else if (action.type === 'FIRST_RESULT') {
-        console.debug('Ignore FIRST_RESULT in ACTIVE state');
-        return state;
-      } else if (action.type === 'SECOND_RESULT') {
-        if (state.goal.type === TwoSpeechbubblesType.ASK_QUESTION
-            && isEqual(state.goal_id, action.value.status.goal_id)) {
-          return {
-            ...state,
-            goal: null,
-            status: Status.SUCCEEDED,
-            result: (action.value as Result).result,
-          };
-        } else {
-          console.debug('Ignore SECOND_RESULT in ACTIVE & !ASK_QUESTION state');
-        return state;
-        }
-      } else if (action.type === 'CANCEL') {
-        return {
-          ...state,
-          goal: null,
-          status: Status.PREEMPTED,
-        };
-      }
-    }
-    console.warn(
-      `Unhandled state.status ${state.status} action.type ${action.type}`
-    );
-    return state;
-  }, initialState);
-
-  // Prepare outgoing streams
-  const stateStatusChanged$ = state$
-    .compose(dropRepeats(
-      (x, y) => (x.status === y.status && isEqual(x.goal_id, y.goal_id))));
-
-  const status$ = stateStatusChanged$
-    .map(state => ({
-      goal_id: state.goal_id,
-      status: state.status,
-    } as GoalStatus));
-
-  const result$ = stateStatusChanged$
-    .filter(state => (state.status === Status.SUCCEEDED
-      || state.status === Status.PREEMPTED
-      || state.status === Status.ABORTED))
-    .map(state => ({
-      status: {
-        goal_id: state.goal_id,
-        status: state.status,
-      },
-      result: state.result,
-    } as Result));
-
-  const goals$ = stateStatusChanged$.filter(state => (
-    state.status === Status.ACTIVE || state.status === Status.PREEMPTED
-  )).map(state => {
-    if (!state.goal) {
-      return {
-        first: null,
-        second: null,
-      };
-    }
-    switch (state.goal.type) {
-      case TwoSpeechbubblesType.SET_MESSAGE:
-        return {
-          first: {
-            goal_id: state.goal_id,
-            goal: {
-              type: SpeechbubbleType.MESSAGE,
-              value: state.goal.value,
-            },
-          },
-          second: null,
-        };
-      case TwoSpeechbubblesType.ASK_QUESTION:
-        return {
-          first: {
-            goal_id: state.goal_id,
-            goal: {
-              type: SpeechbubbleType.MESSAGE,
-              value: state.goal.value.message,
-            },
-          },
-          second: {
-            goal_id: state.goal_id,
-            goal: {
-              type: SpeechbubbleType.CHOICE,
-              value: state.goal.value.choices,
-            },
-          },
-        };
-    };
-  });
-  const firstGoal$ = goals$.map(state => state.first);
-  const secondGoal$ = goals$.map(state => state.second);
-
-  return {
-    DOM: adapt(xs.combine(first.DOM, second.DOM).map(([fdom, sdom]) => (
-      <div>
-        <div>
-          <span>Robot:</span> <span>{fdom}</span>
-        </div>
-        <div>
-          <span>Human:</span> <span>{sdom}</span>
-        </div>
-      </div>
-    ))),
-    status: adapt(status$),
-    result: adapt(result$),
-    targets: {
-      firstGoal: firstGoal$,
-      secondGoal: secondGoal$
+function createTraxnsition() {
+  const transitionTable = {
+    [State.DONE]: {
+      [InputType.GOAL]: () => {},
     },
   };
+
+  return function(state, variables, input) {
+    return !transitionTable[state]
+      ? state
+      : !transitionTable[state][input.type]
+        ? state
+        : transitionTable[state][input.type](variables, input.value);
+  }
 }
 
-function powerup(main, connect) {
-  return (sources) => {
-    const sinks = main(sources);
-    Object.keys(sources.proxies).map(key => {
-      connect(sources.proxies[key], sinks.targets[key]);
+function transitionReducer(input$: Stream<Input>): Stream<Reducer> {
+  const initReducer$: Stream<Reducer> = xs.of(
+    function initReducer(machine: Machine): Machine {
+      return {
+        state: State.DONE,
+        variables: {
+          goal_id: null,
+          goal: null,
+          newGoal: null,
+        },
+        outputs: null,
+      }
+    }
+  );
+
+  const transition = createTraxnsition();
+  const inputReducer$: Stream<Reducer> = input$
+    .map(input => function inputReducer(machine: Machine): Machine {
+      return transition(machine.state, machine.variables, input);
     });
-    const {targets, ...sinksNoTargets} = sinks;
-    return sinksNoTargets;
+
+  return xs.merge(initReducer$, inputReducer$);
+}
+
+function output(machine$) {
+  const outputs$ = machine$
+    .filter(machine => !!machine.outputs)
+    .map(machine => machine.outputs);
+
+  return {
+    result: adapt(outputs$
+      .filter(outputs => !!outputs.result)
+      .map(outputs => outputs.result)
+    ),
   };
 }
 
-export function TwoSpeechbubblesAction(sources) {
-  return powerup(main, (proxy, target) => proxy.imitate(target))(sources);
+export function TwoSpeechbubblesAction(sources: {
+  goal: any,
+}): {
+  result: any,
+} {
+  const input$ = input(xs.fromObservable(sources.goal));
+
+  const machine$ = transitionReducer(input$)
+    .fold((state: Machine, reducer: Reducer) => reducer(state), null)
+    .drop(1);  // drop "null";
+
+  const sinks = output(machine$);
+  return sinks;
 }
 
 export function IsolatedTwoSpeechbubblesAction(sources) {
