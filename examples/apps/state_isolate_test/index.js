48850fc4a22018bdc0dc88561610aafb049f1fe3
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-12-15 11:55:18 -0800

    WIP: refactor Counter to function

--- examples/apps/state_isolate_test/index.js
+++ examples/apps/state_isolate_test/index.js
@@ -4,14 +4,14 @@ import isolate from '@cycle/isolate';
 import {div, button, p, makeDOMDriver} from '@cycle/dom';
 import {withState} from '@cycle/state';
 
-function main(sources) {
+function Counter(sources) {
   const action$ = xs.merge(
     sources.DOM.select('.decrement').events('click').map(ev => -1),
     sources.DOM.select('.increment').events('click').map(ev => +1)
   );
 
   const state$ = sources.state.stream;
-  state$.addListener({next: v => console.log('state', v)});
+  state$.addListener({next: v => console.log(v)})
 
   const vdom$ = state$.map(state =>
     div([
@@ -21,20 +21,29 @@ function main(sources) {
     ])
   );
 
-  const initReducer$ = xs.of(function initReducer() {
-    return {count: 0};
+  const initReducer$ = xs.of(function initReducer(prevState) {
+    if (prevState) {
+      return prevState;
+    } else {
+      return {count: 0};
+    }
   });
-
-  const updateReducer$ = action$.map(num => function updateReducer(prevState) {
-    return {count: prevState.count + num};
-  });
-
+  const updateReducer$ = action$
+    .map(num => function updateReducer(prevState) {
+      return {count: prevState.count + num};
+    });
   const reducer$ = xs.merge(initReducer$, updateReducer$);
 
   return {
     DOM: vdom$,
     state: reducer$,
   };
+}
+
+function main(sources) {
+  const sinks = Counter(sources);
+
+  return sinks;
 }
 
 const wrappedMain = withState(main);
