18b6deaab9c7e86d234f8237b2e6fd7e2f2f6edb
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-12-17 21:51:48 -0800

    Working brfs!

--- examples/apps/hfsm/index.js
+++ examples/apps/hfsm/index.js
@@ -5,6 +5,9 @@ import {withState} from '@cycle/state'
 import {runRobotProgram} from '@cycle-robot-drivers/run';
 import FlowchartAction from './FlowchartAction';
 
+var fs = require('fs');
+console.log(fs.readFileSync('/tmp/test.txt', 'utf8'));
+
 const Sentence = {
   CAREER: 'Is it important that you reach your full career potential?',
   ONLINE: 'Can you see yourself working online?',
