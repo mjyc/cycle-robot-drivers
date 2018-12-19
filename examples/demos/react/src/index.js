6f8b8a996c5f8b98052901520d547417191ba049
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-12-19 00:03:17 -0800

    Finish initial react+cycle-robot-drivers demo

--- examples/demos/react/src/index.js
+++ examples/demos/react/src/index.js
@@ -2,12 +2,14 @@ import xs from "xstream";
 import delay from "xstream/extra/delay";
 import React from "react";
 import ReactDOM from "react-dom";
-import { makeComponent } from "@cycle/react";
-import { makeTabletFaceDriver } from "@cycle-robot-drivers/screen";
+import {makeComponent} from "@cycle/react";
+import {makeTabletFaceDriver} from "@cycle-robot-drivers/screen";
 
 function robotFace(sources) {
   return {
-    TabletFace:xs.of({type: 'EXPRESS', value: 'happy'}).compose(delay(2000)),
+    TabletFace:xs.of({
+      type: 'EXPRESS', value: {type: 'happy'}
+    }).compose(delay(2000)),
     react: sources.TabletFace.react,
   }
 }
@@ -19,7 +21,6 @@ const RobotFace = makeComponent(robotFace, {
 function App(props) {
   return (
     <div className="app">
-      <Highlighter text={"The book is on the table."} />
       <RobotFace />
     </div>
   );
