d8de2527cdb461e6b5e296b78815a39b587a859f
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-12-27 12:24:09 -0800

    Update example

--- examples/demos/meyda/src/index.ts
+++ examples/demos/meyda/src/index.ts
@@ -1,11 +1,41 @@
+import xs from 'xstream';
+import {div, makeDOMDriver} from '@cycle/dom';
 import {run} from '@cycle/run';
 import {makeMeydaDriver} from 'cycle-meyda-driver';
 
 function main(sources) {
   sources.Meyda.addListener({next: f => console.log(f)});
-  return {};
+
+  const vdom$ = sources.Meyda.map(features => div(
+    Object.keys(features).map(
+      key => div(`${key}: ${JSON.stringify(features[key])}`
+    ))
+  ));
+  return {
+    DOM: vdom$,
+  };
 }
 
 run(main, {
-  Meyda: makeMeydaDriver(),
+  DOM: makeDOMDriver('#app'),
+  Meyda: makeMeydaDriver({
+    featureExtractors: [
+      'rms',
+      'energy',
+      'spectralSlope',
+      'spectralCentroid',
+      'spectralRolloff',
+      'spectralFlatness',
+      'spectralSpread',
+      'spectralSkewness',
+      'spectralKurtosis',
+      'zcr',
+      'loudness',
+      'perceptualSpread',
+      'perceptualSharpness',
+      'mfcc',
+      'chroma',
+      'powerSpectrum',
+    ],
+  }),
 });
