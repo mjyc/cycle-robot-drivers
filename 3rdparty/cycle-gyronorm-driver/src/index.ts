1f6dd25b328eff218452ef2456b0ae4eeba1deec
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-12-27 11:21:12 -0800

    WIP: test if gyronorm is working

--- 3rdparty/cycle-gyronorm-driver/src/index.ts
+++ 3rdparty/cycle-gyronorm-driver/src/index.ts
@@ -1,12 +1,38 @@
 import xs from 'xstream';
-import GyroNorm from 'gyronorm';
-console.log(GyroNorm);
+// import '@hughsk/fulltilt/dist/fulltilt';
+// console.log((window as any).FULLTILT);
+// (window as any).FULLTILT.getDeviceOrientation({'type': 'game'});
+// (window as any).FULLTILT.getDeviceMotion();
+// import GyroNorm from 'gyronorm';
+declare var require: any
+var GyroNorm = require('gyronorm/dist/gyronorm.complete');
+// console.log(GyroNorm);
+
+// import * as GyroNorm from './gyronorm.complete';
+// const GyroNorm = require('./gyronorm.complete');
+// console.log(GyroNorm);
 
 import {adapt} from '@cycle/run/lib/adapt';
 
 export function makeGyronormDriver() {
-  return function() {
-    const source$ = xs.periodic(100);
+  const gn = new GyroNorm();
+
+  function gyronormDriver() {
+    // const source$ = xs.periodic(1000);
+    const source$ = xs.create({
+      start: listener => {
+        gn.init().then(() => {
+          gn.start((data) => {
+            listener.next(data);
+          });
+        });
+      },
+      stop: () => {
+        gn.stop();
+      }
+    });
     return adapt(source$);
   }
+
+  return gyronormDriver;
 }
