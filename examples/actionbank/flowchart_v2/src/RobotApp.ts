cef70a57dcdd6bdfc7bef067962268e241772ede
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-01-04 11:33:41 -0800

    Update flowchart_v2 to use run pkg

--- examples/actionbank/flowchart_v2/src/RobotApp.ts
+++ examples/actionbank/flowchart_v2/src/RobotApp.ts
@@ -1,68 +1,22 @@
 import xs from 'xstream';
-import {Stream} from 'xstream';
-import {div, DOMSource, VNode} from '@cycle/dom';
 import isolate from '@cycle/isolate';
-import {StateSource, Reducer} from '@cycle/state';
-import {Result, EventSource, isEqual, Status} from '@cycle-robot-drivers/action';
-import {
-  TwoSpeechbubblesAction,
-} from '@cycle-robot-drivers/screen';
-import {
-  SpeechSynthesisAction,
-  SpeechRecognitionAction,
-} from '@cycle-robot-drivers/speech';
-import {selectActionResult} from '@cycle-robot-drivers/actionbank';
-import {
-  TwoSpeechbuttonsActionSinks as TWASinks,
-  SpeechSynthesisActionSinks as SSSinks,
-  SpeechRecogntionActionSinks as SRSinks,
-} from './types';
+import {Result, isEqual, Status} from '@cycle-robot-drivers/action';
 import {FlowchartAction} from './FlowchartAction';
 
-export interface State {
-  FacialExpressionAction: {result: Result},
-  TwoSpeechbubblesAction: {result: Result},
-  SpeechSynthesisAction: {result: Result},
-  SpeechRecognitionAction: {result: Result},
-}
+export default function RobotApp(sources) {
+  // sources.state.stream.addListener({next: s => console.log(s)})
 
-export interface Sources {
-  DOM: DOMSource,
-  TabletFace: any,
-  SpeechSynthesis: EventSource,
-  SpeechRecognition: EventSource,
-  state: StateSource<State>,
-}
-
-export interface Sinks {
-  DOM: Stream<VNode>,
-  SpeechSynthesis: any,
-  SpeechRecognition: any,
-  state: Stream<Reducer<State>>,
-}
-
-export default function RobotApp(sources: Sources): Sinks {
-  // sources.state.stream.addListener({next: v => console.log('state$', v)})
-
-  // Process state stream
-  const state$ = sources.state.stream;
-  const twoSpeechbubblesResult$ = state$
-    .compose(selectActionResult('TwoSpeechbubblesAction'));
-  const speechSynthesisResult$ = state$
-    .compose(selectActionResult('SpeechSynthesisAction'));
-  const speechRecognitionResult$ = state$
-    .compose(selectActionResult('SpeechRecognitionAction'));
-
-
-  // "main" component
+  // fetch flowchart data
   const flowchartMetadata = [
     {
       name: 'Broccoli cheese soup',
       path: '/flowcharts/crockpot-buffalo-chicken.json',
     },
+    {
+      name: 'Downtown Marinade',
+      path: '/flowcharts/downtown-chowdown-marinade.json',
+    },
   ];
-
-  // fetch flowchart data
   const data$ = xs.combine.apply(
     null,
     flowchartMetadata.map(d => xs.fromPromise(fetch(d.path, {
@@ -74,8 +28,10 @@ export default function RobotApp(sources: Sources): Sinks {
       ...j,
     }))).flatten()),
   );
+
+  // setup the flowchart action
   const goalId = {stamp: new Date(), id: '#main-screen'};
-  const goal$ = xs.combine(data$, twoSpeechbubblesResult$)
+  const goal$ = xs.combine(data$, sources.TwoSpeechbubblesAction.result)
     .filter(([data, r]: [any[], Result]) =>  // filter not #main-screen results
       isEqual(r.status.goal_id, goalId)
       && r.status.status !== Status.PREEMPTED
@@ -84,11 +40,11 @@ export default function RobotApp(sources: Sources): Sinks {
       flowchart: data.filter(d => d.name === r.result)[0].flowchart,
       start_id: data.filter(d => d.name === r.result)[0].start_id,
     }));
-  const childSinks: any = isolate(FlowchartAction)({
+  const fcSinks: any = isolate(FlowchartAction, 'FlowchartAction')({
     goal: goal$,
-    TwoSpeechbubblesAction: {result: twoSpeechbubblesResult$},
-    SpeechSynthesisAction: {result: speechSynthesisResult$},
-    SpeechRecognitionAction: {result: speechRecognitionResult$},
+    TwoSpeechbubblesAction: {result: sources.TwoSpeechbubblesAction.result},
+    SpeechSynthesisAction: {result: sources.SpeechSynthesisAction.result},
+    SpeechRecognitionAction: {result: sources.SpeechRecognitionAction.result},
     state: sources.state,
   });
 
@@ -100,54 +56,23 @@ export default function RobotApp(sources: Sources): Sinks {
       choices: data.map(d => d.name),
     }
   }));
+
+  // define sinks
   const twoSpeechbubblesGoal$ = xs.merge(
-    xs.combine(mainScreen$, childSinks.result.startWith(null)).map(x => x[0]),
-    childSinks.TwoSpeechbubblesAction,
+    xs.combine(mainScreen$, fcSinks.result.startWith(null)).map(x => x[0]),
+    fcSinks.TwoSpeechbubblesAction,
   );
-
-
-  // Define Actions
-  const twoSpeechbubblesAction: TWASinks = TwoSpeechbubblesAction({
-    goal: twoSpeechbubblesGoal$,
-    DOM: sources.DOM,
-  });
-  const speechSynthesisAction: SSSinks = SpeechSynthesisAction({
-    goal: childSinks.SpeechSynthesisAction || xs.never(),
-    SpeechSynthesis: sources.SpeechSynthesis,
-  });
-  const speechRecognitionAction: SRSinks = SpeechRecognitionAction({
-    goal: childSinks.SpeechRecognitionAction || xs.never(),
-    SpeechRecognition: sources.SpeechRecognition,
-  });
-
-
-  // Define Reducers
-  const parentReducer$: Stream<Reducer<State>> = xs.merge(
-    twoSpeechbubblesAction.result.map(result =>
-      prev => ({...prev, TwoSpeechbubblesAction: {outputs: {result}}})),
-    speechSynthesisAction.result.map(result =>
-      prev => ({...prev, SpeechSynthesisAction: {outputs: {result}}})),
-    speechRecognitionAction.result.map(result =>
-      prev => ({...prev, SpeechRecognitionAction: {outputs: {result}}})),
-  );
-  const childReducer$: Stream<Reducer<State>> = childSinks.state;
-  const reducer$ = xs.merge(parentReducer$, childReducer$);
-
-
-  // Define Sinks
-  const vdom$ = xs.combine(
-    twoSpeechbubblesAction.DOM,
-    sources.TabletFace.DOM,
-  ).map(([speechbubbles, face]) =>
-    div({
-      style: {position: 'relative'}
-    }, [speechbubbles, face])
-  );
-
-  return {
-    DOM: vdom$,
-    SpeechSynthesis: speechSynthesisAction.output,
-    SpeechRecognition: speechRecognitionAction.output,
+  const reducer$: any = fcSinks.state;
+  const sinks = {
+    TwoSpeechbubblesAction: twoSpeechbubblesGoal$,
+    SpeechSynthesisAction: fcSinks.SpeechSynthesisAction,
+    SpeechRecognitionAction: fcSinks.SpeechRecognitionAction,
+    PoseDetection: xs.of({
+      algorithm: 'single-pose',
+      singlePoseDetection: {minPoseConfidence: 0.2},
+    }),
     state: reducer$,
   };
+
+  return sinks;
 }
