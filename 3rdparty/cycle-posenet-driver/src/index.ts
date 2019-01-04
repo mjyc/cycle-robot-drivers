23aea45338d4ed40949ca458d41232ba91e805d7
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-01-04 10:29:26 -0800

    Export "Keypoint, Pose" types from tfjs posenet

--- 3rdparty/cycle-posenet-driver/src/index.ts
+++ 3rdparty/cycle-posenet-driver/src/index.ts
@@ -1,1 +1,2 @@
-export {makePoseDetectionDriver, PoseNetParameters} from './makePoseDetectionDriver'
+export {makePoseDetectionDriver, PoseNetParameters} from './makePoseDetectionDriver';
+export {Keypoint, Pose} from '@tensorflow-models/posenet';
