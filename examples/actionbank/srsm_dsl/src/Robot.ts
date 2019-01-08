236fee02de45122cb937dc76afa1a87b669c0b43
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-01-07 23:32:23 -0800

    Initial interp integration

---
+++ examples/actionbank/srsm_dsl/src/Robot.ts
@@ -0,0 +1,127 @@
+import xs from 'xstream';
+import delay from 'xstream/extra/delay';
+import dropRepeats from 'xstream/extra/dropRepeats';
+import parser from './parser';
+import {Status, isEqualGoal, initGoal} from '@cycle-robot-drivers/action';
+
+
+const testCode = `
+S1[speak "hello"] -> |speakDone| S2
+S2[speak "how are you?"] -> |speakDone| S3
+S3[speak "okay bye"] -> |speakDone| S4
+`;
+
+const testTree = parser.parse(testCode);
+
+console.log(testTree);
+
+
+
+function interp(tree) {
+  if (tree.type === 'speakDone') {
+    return tree;
+  } else if (tree.type === 'input') {
+    return interp(tree.value);  // TODO: support arrays
+  } else if (tree.type === 'speak') {
+    return {SpeechSynthesisAction: {goal: initGoal(tree.value)}};
+  } else if (tree.type === 'action') {
+    return {outputs: interp(tree.value)};  // TODO: support arrays
+  } else if (tree.type === 'state') {
+    return tree.value;
+  } else if (tree.type === 'transition') {
+    return (prev, input) =>
+      (prev.s === interp(tree.value[0]) && input.type === interp(tree.value[2]).type)
+        ? {
+            ...prev,
+            s: interp(tree.value[3]),
+            o: interp(tree.value[1]).outputs,
+          }
+        : null;
+  } else if (tree.type === 'fsm') {
+    return (prev, input) =>
+      tree.value.reduceRight((acc, x) => {
+        const v = interp(x)(prev, input);
+        return !!v ? v : acc;
+      }, prev);
+  } else {
+    return null;
+  }
+}
+
+const trans = interp(testTree);
+
+console.log(trans);
+
+
+function input(
+  speechSynthesisResult$,
+  twoSpeechbubblesResult$,
+) {
+  return xs.merge(
+    speechSynthesisResult$
+      .filter(r => r.status.status === Status.SUCCEEDED)
+      .mapTo({type: 'speakDone'}).compose(delay(200)),
+    twoSpeechbubblesResult$
+      .filter(r => r.status.status === Status.SUCCEEDED)
+      .mapTo({type: 'askQuestionDone'}),
+  );
+}
+
+function reducer(input$) {
+  const initReducer$ = xs.of((prev) => {
+    if (typeof prev === 'undefined') {
+      return {
+        s: 'S1',
+        o: {SpeechSynthesisAction: {goal: initGoal('Hello')}},
+      };
+    } else {
+      return prev;
+    }
+  });
+
+  const transitionReducer = input$.map((input) => (prev) => {
+    console.debug('input', input, 'prev', prev);
+    return trans(prev,input);
+  });
+
+  return xs.merge(initReducer$, transitionReducer);
+}
+
+function output(reducerState$) {
+  const outputs$ = reducerState$
+    .filter(m => !!m.o)
+    .map(m => m.o);
+  return {
+    // result: outputs$
+    //   .filter(o => !!o.result)
+    //   .map(o => o.result)
+    //   .compose(dropRepeats(isEqualResult)),
+    SpeechSynthesisAction: outputs$
+      .filter(o => !!o.SpeechSynthesisAction)
+      .map(o => o.SpeechSynthesisAction.goal)
+      .compose(dropRepeats(isEqualGoal)),
+  };
+}
+
+export default function Robot(sources) {
+
+  const input$ = input(
+    sources.SpeechSynthesisAction.result,
+    sources.TwoSpeechbubblesAction.result,
+  );
+
+  const reducer$ = reducer(input$);
+
+  const reducerState$ = sources.state.stream.debug();
+  reducerState$.addListener({next: s => console.warn(s)});
+  const outputs = output(reducerState$);
+
+  return {
+    ...outputs,
+    PoseDetection: xs.of({
+      algorithm: 'single-pose',
+      singlePoseDetection: {minPoseConfidence: 0.2},
+    }),
+    state: reducer$,
+  };
+}
--- examples/actionbank/srsm_dsl/src/index.ts
+++ examples/actionbank/srsm_dsl/src/index.ts
@@ -1,100 +1,26 @@
 import xs from 'xstream';
 import mermaid from "mermaid";
-import pegjs from 'pegjs';
 import {div} from '@cycle/dom';
 import {run} from '@cycle/run';
 import {withState} from '@cycle/state';
 import {initializeDrivers, withRobotActions} from '@cycle-robot-drivers/run';
 import {makeTabletFaceDriver} from '@cycle-robot-drivers/screen';
-
-const grammar = `
-fsm
-  = _ head:transition tail:(brp transition)* _ {
-      return {
-        type: "fsm",
-        value: tail.reduce(function(acc, x) {
-          return acc.concat(x[1]);
-        }, [head])
-      };
-    }
-
-transition
-  = ws from:state ws a:action ws "->" ws i:input ws to:state {
-      return {
-        type: "transition",
-        value: [from, a, i, to]
-      };
-    }
-
-state
-  = id:[a-zA-Z0-9]+ {
-      return {type: "state", value: id.join("")};
-    }
-
-action
-  = "[" ws action:(speak / displayMessage) ws "]" { return action; }
-
-speak
-  = "speak" ws "\\"" arg:[a-zA-Z0-9']+ "\\"" {
-      return {
-        type: "speak",
-        value: arg.join("")
-      };
-    }
-
-displayMessage
-  = "displayMessage" ws "\\"" arg:[a-zA-Z0-9']+ "\\"" {
-      return {
-        type: "displayMessage",
-        value: arg.join("")
-      };
-    }
-
-input
-  = "|" ws input:(speakDone / displayMessageDone) ws "|" { return input; }
-
-speakDone
-  = "speakDone" {
-      return {
-        type: "speakDone",
-        value: null
-      };
-    }
-
-displayMessageDone
-  = "displayMessageDone" {
-      return {
-        type: "displayMessageDone",
-        value: null
-      };
-    }
-
-_ "blank" = [ \\t\\r\\n]*
-
-ws "whitespace" = [ \\t]*
-
-brp "linebreak" = [\\r\\n]+
-`;
-
-function Robot(sources) {
-  return {
-    PoseDetection: xs.of({
-      algorithm: 'single-pose',
-      singlePoseDetection: {minPoseConfidence: 0.2},
-    }),
-  }
-}
+import Robot from './Robot';
+import parser from './parser';
 
 function toMermaid(node) {
   if (node.type === 'transition') {
-    return node.value[0].value + '[\"'  + node.value[1].value + '\"]'
-      + '--> |' + node.value[2].value + '| ' + node.value[3].value;
+    return node.value[0].value
+      + '[\"'  + node.value[1].value.type + ': ' + node.value[1].value.value + '\"]'
+      + '--> |' + node.value[2].value.type + '| ' + node.value[3].value;
   } else if (node.type === 'fsm') {
     return node.value.map(toMermaid).join('\n');
   }
 }
 
 function main(sources) {
+  sources.state.stream.addListener({next: s => console.warn(s)});
+
   document.body.style.backgroundColor = 'white';
   document.body.style.margin = '0px';
 
@@ -103,7 +29,6 @@ function main(sources) {
     'content-type': 'text/plain'
   }})).map(v => xs.fromPromise(v.text())).flatten();
 
-  const parser = pegjs.generate(grammar);
   const sinks: any = withRobotActions(Robot, {hidePoseViz: true})(sources);
   const vdom$ = xs.combine(sinks.DOM, code$).map(([face, code]) => {
     let ast;
@@ -115,7 +40,11 @@ function main(sources) {
 ${'-'.repeat(3+e.location.start.column-1) + '^'}
 ${e.message}`);
     }
-    return div([div('#graphDiv', 'graph TB\n' + toMermaid(ast)), face]);
+    console.log('toMermaid(ast)', toMermaid(ast));
+    return div([
+      face,
+      div('#graphDiv', 'graph TB\n' + toMermaid(ast)),
+    ]);
   });
 
   // render code
