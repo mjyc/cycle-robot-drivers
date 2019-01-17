b0490533864ba24515e410c168eb99082710c65c
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-01-16 22:09:45 -0800

    WIP: replay exp

--- examples/actionbank/record/src/index.ts
+++ examples/actionbank/record/src/index.ts
@@ -1,4 +1,5 @@
 import xs from 'xstream';
+import dropRepeats from 'xstream/extra/dropRepeats';
 import {div, a} from '@cycle/dom';
 
 import {withState} from '@cycle/state';
@@ -8,15 +9,64 @@ import {withRobotActions, initializeDrivers} from '@cycle-robot-drivers/run';
 import Robot from './Robot';
 import Bagger from './Bagger';
 
-import 'semantic-ui-css/semantic.min.css';
+// import 'semantic-ui-css/semantic.min.css';
 
 // document.body.style.backgroundColor = "white";
 // document.body.style.margin = "0px";
 
 function main(sources) {
+
+  let data = null;
+  let data2 = null;
+  xs.fromPromise(fetch('/f0577c65-425f-4331-8ff1-5c0ac05c1574.json', {
+    headers: {
+      "content-type": "application/json"
+    }
+  })).map(v => xs.fromPromise(v.json())).flatten().addListener({next: v => {
+    data = v.DOM; data2 = v.SpeechSynthesis; console.log(v);}});
+
+  // const time$ = sources.Time.animationFrames().map(frame => frame.time);
+  // time$
+  //   .addListener({
+  //     next: (time) => {
+  //       if (!!data) {
+  //         console.log(
+  //           data.slice(0).reverse()
+  //             .find(val => val[1] <= time)
+  //         );
+  //       }
+  //     }
+  //   });
+
+  const time$ = sources.Time.animationFrames();
+  const vdomExp$ = time$.map(frame => frame.time)
+    .map(time => {
+      if (!!data) {
+        // console.log(
+        const found = data.slice(0).reverse()
+            .find(val => val[1] <= time)
+        // );
+        if (!!found) return found[0]
+      }
+    }).filter(x => !!x).compose(dropRepeats()).startWith(div('hello'));
+  const speech$ = time$.map(frame => frame.time)
+    .map(time => {
+      if (!!data2) {
+        // console.log(
+        const found = data2.slice(0).reverse()
+            .find(val => val[1] <= time)
+        // );
+        if (!!found) return found[0]
+      }
+    }).filter(x => !!x).compose(dropRepeats());
+
+
   const sinks: any = withRobotActions(Robot)(sources);
 
-  const dataUrl$ = Bagger({DOM: sinks.DOM}, sources.Time)
+  const dataUrl$ = Bagger({
+    DOM: sinks.DOM,
+    SpeechSynthesis: sinks.SpeechSynthesis,
+  }, sources.Time)
     .map(data => {
       const blob = new Blob([JSON.stringify(data, null, 2)], {type : 'application/json'});
       const url = window.URL.createObjectURL(blob);
@@ -35,7 +85,9 @@ function main(sources) {
 
   return {
     ...sinks,
-    DOM: vdom$,
+    // DOM: vdom$,
+    DOM: vdomExp$,
+    SpeechSynthesis: speech$,
   };
 }
 
