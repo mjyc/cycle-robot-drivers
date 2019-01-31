3ca3b217f9af6514bc87ef4e13318b345f4af194
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-01-30 19:33:05 -0800

    Finish faintheart

--- examples/simple/fainthearted/src/index.js
+++ examples/simple/fainthearted/src/index.js
@@ -1,24 +1,36 @@
 import {run} from '@cycle/run';
 import {div, label, input, hr, h1, makeDOMDriver} from '@cycle/dom';
+import {makeMeydaDriver} from 'cycle-meyda-driver';
+
+function makeVibrationDriver() {
+  const vibrationDriver = (sink$) => {
+    sink$.addListener({
+      next: (pattern) => {
+        window.navigator.vibrate(pattern);
+      }
+    });
+  };
+
+  return vibrationDriver;
+}
 
 function main(sources) {
-  const vdom$ = sources.DOM
-    .select('.myinput').events('input')
-    .map(ev => ev.target.value)
-    .startWith('')
-    .map(name =>
-      div([
-        label('Name:'),
-        input('.myinput', {attrs: {type: 'text'}}),
-        hr(),
-        h1(`Hello ${name}`)
-      ])
-    );
+  const vdom$ = sources.Meyda
+    .map(v => div(`loudness: ${v.loudness.total}`));
+
+  const vibration$ = sources.Meyda.filter(v => v.loudness.total > 50)
+    .mapTo(500);
+
   return {
     DOM: vdom$,
+    Vibration: vibration$,
   };
 }
 
 run(main, {
-  DOM: makeDOMDriver('#app')
+  DOM: makeDOMDriver('#app'),
+  Meyda: makeMeydaDriver({
+    featureExtractors: ['loudness']
+  }),
+  Vibration: makeVibrationDriver(),
 });
