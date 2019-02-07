9eee1374c8f60bda9dc573930370da6eb4ef00b2
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-02-06 18:28:02 -0800

    Update speech demo example

--- examples/demos/speech/src/index.js
+++ examples/demos/speech/src/index.js
@@ -1,42 +1,55 @@
 import xs from 'xstream';
+import sampleCombine from 'xstream/extra/sampleCombine';
 import {run} from '@cycle/run';
 import {withState} from '@cycle/state';
 import {div, label, input, br, button, makeDOMDriver} from '@cycle/dom';
 import {
   makeSpeechSynthesisDriver,
-  SpeechSynthesisAction2,
+  SpeechSynthesisAction,
   makeSpeechRecognitionDriver,
 } from '@cycle-robot-drivers/speech';
 
 function main(sources) {
-  const vdom$ = sources.DOM
-    .select('.myinput').events('input')
+  const say$ = sources.DOM.select('.say').events('click');
+  const inputText$ = sources.DOM
+    .select('.inputtext').events('input')
     .map(ev => ev.target.value)
-    .startWith('')
-    .map(name =>
-      div([
-        button('.say', 'say'),
-        input('.myinput', {attrs: {type: 'text'}}),
-        br(),
-        button('listen'),
-        label('heard: hello!')
-      ])
-    );
-
-  const click$ = sources.DOM.select('.say').events('click');
-
-  const speechSynthesisAction = SpeechSynthesisAction2({
+    .startWith('');
+  const synthGoal$ = say$.compose(sampleCombine(inputText$))
+    .filter(([_, text]) => !!text)
+    .map(([_, text]) => ({goal_id: `${new Date().getTime()}`, goal: text}));
+  const speechSynthesisAction = SpeechSynthesisAction({
     state: sources.state,
-    goal: click$.mapTo({goal_id: 'test', goal: 'Hello'}), //xs.never(),
+    goal: synthGoal$,
     cancel: xs.never(),
     SpeechSynthesis: sources.SpeechSynthesis,
   });
+  speechSynthesisAction.status.addListener({next: s =>
+    console.log('SpeechSynthesisAction status', s)});
 
-  sources.state.stream.addListener({next: s => console.log(s)});
+  // const recogGoal$ = sources.DOM.select('.listen').events('click').mapTo({});
+  // const speechRecognitionAction = SpeechRecognitionAction({
+  //   state: sources.state,
+  //   goal: recogGoal$,
+  //   cancel: xs.never(),
+  //   SpeechRecognition: sources.SpeechRecognition,
+  // });
+  // speechRecognitionAction.status.addListener({next: s =>
+  //   console.log('SpeechRecognitionAction status', s)});
+
+
+  const vdom$ = xs.of(div([
+    button('.say', 'say'),
+    input('.inputtext', {attrs: {type: 'text'}}),
+    br(),
+    button('.listen', 'listen'),
+    label('heard: hello!')
+  ]));
 
   return {
     DOM: vdom$,
     SpeechSynthesis: speechSynthesisAction.SpeechSynthesis,
+    // SpeechRecognition: speechRecognitionAction.SpeechRecognition,
     state: speechSynthesisAction.state,
   };
 }
