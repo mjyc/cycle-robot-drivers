20b47c465a185e43a12c89ead2f2591dce939b85
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-02-22 15:29:39 -0800

    Styling

--- examples/actionbank/engagement_management/src/index.ts
+++ examples/actionbank/engagement_management/src/index.ts
@@ -1,11 +1,7 @@
+document.body.style.backgroundColor = "white";
+document.body.style.margin = "0px";
+
 import {runRobotProgram} from '@cycle-robot-drivers/run';
 import EngagementManagement from './EngagementManagement';
 
-function main(sources) {
-  document.body.style.backgroundColor = "white";
-  document.body.style.margin = "0px";
-
-  return EngagementManagement(sources);
-}
-
-runRobotProgram(main);
+runRobotProgram(EngagementManagement);
