a7dbde74635b2e67f8c1b77987b42fca4e7e7c5e
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-04-03 14:36:42 -0700

    Update actionbank

--- actionbank/src/index.ts
+++ actionbank/src/index.ts
@@ -1,6 +1,4 @@
-export {Omit, FSMReducerState} from './types';
 export {
-  Sources as QuestionAnswerActionSources,
-  Sinks as QuestionAnswerActionSinks,
-  QuestionAnswerAction,
-} from './QuestionAnswerAction';
+  createTwoSpeechbubbles,
+  TwoSpeechbubbles,
+} from './TwoSpeechbubbles';
