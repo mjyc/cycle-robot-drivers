13fbebafdeade04bc87c8df65a5c6361f0a8ece5
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-04-03 10:29:14 -0700

    Remove outdated actionbank examples

--- examples/actionbank/flowchart/src/index.ts
+++
@@ -1,7 +0,0 @@
-import {runRobotProgram} from '@cycle-robot-drivers/run';
-import Robot from './Robot';
-
-document.body.style.backgroundColor = "white";
-document.body.style.margin = "0px";
-
-runRobotProgram(Robot);
