232e5dfe3345bf05236413b11fc878347f030a80
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-04-03 10:40:47 -0700

    Remove unused actions

--- actionbank/src/index.ts
+++ actionbank/src/index.ts
@@ -1,23 +1,6 @@
 export {Omit, FSMReducerState} from './types';
-export {selectActionResult} from './utils';
-export {makeConcurrentAction} from './makeConcurrentAction';
-export {
-  Sources as SpeakWithScreenActionSources,
-  Sinks as SpeakWithScreenActionSinks,
-  SpeakWithScreenAction,
-} from './SpeakWithScreenAction';
 export {
   Sources as QuestionAnswerActionSources,
   Sinks as QuestionAnswerActionSinks,
   QuestionAnswerAction,
 } from './QuestionAnswerAction';
-export {
-  Sources as QAWithScreenActionSources,
-  Sinks as QAWithScreenActionSinks,
-  QAWithScreenAction,
-} from './QAWithScreenAction';
-export {
-  Sources as FlowchartActionSources,
-  Sinks as FlowchartActionSinks,
-  FlowchartAction,
-} from './FlowchartAction';
