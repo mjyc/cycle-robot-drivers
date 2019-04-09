9fb9789baa6283bc2cd3a01aaead13939d2085fb
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-04-09 09:00:54 -0700

    WIP

--- examples/apps/template/src/index.js
+++ examples/apps/template/src/index.js
@@ -1,7 +1,7 @@
 import xs from 'xstream';
 import delay from 'xstream/extra/delay';
 import {runTabletFaceRobotApp} from '@cycle-robot-drivers/run';
-import {initGoal, selectActionResult} from '@cycle-robot-drivers/action';
+import {initGoal} from '@cycle-robot-drivers/action';
 
 
 function input({
@@ -13,72 +13,103 @@ function input({
   SpeechRecognitionAction,
   PoseDetection,
 }) {
-  return [
-    xs.merge(
-      FacialExpressionAction.result.map(r => ({
-        type: 'FacialExpressionAction',
-        ...r,
-      })),
-      RobotSpeechbubbleAction.result.map(r => ({
-        type: 'RobotSpeechbubbleAction',
-        ...r,
-      })),
-      HumanSpeechbubbleAction.result.map(r => ({
-        type: 'HumanSpeechbubbleAction',
-        ...r,
-      })),
-      AudioPlayerAction.result.map(r => ({
-        type: 'AudioPlayerAction',
-        ...r,
-      })),
-      SpeechSynthesisAction.result.map(r => ({
-        type: 'SpeechSynthesisAction',
-        ...r,
-      })),
-      SpeechRecognitionAction.result.map(r => ({
-        type: 'SpeechRecognitionAction',
-        ...r,
-      })),
-    ),
-    xs.never(),
-    // PoseDetection.events('poses').map(poses => ({
-    // })),
-  ];
+  const inputD$ = xs.merge(
+    FacialExpressionAction.result.map(r => ({
+      type: 'FacialExpressionAction',
+      ...r,
+    })),
+    RobotSpeechbubbleAction.result.map(r => ({
+      type: 'RobotSpeechbubbleAction',
+      ...r,
+    })),
+    HumanSpeechbubbleAction.result.map(r => ({
+      type: 'HumanSpeechbubbleAction',
+      ...r,
+    })),
+    AudioPlayerAction.result.map(r => ({
+      type: 'AudioPlayerAction',
+      ...r,
+    })),
+    SpeechSynthesisAction.result.map(r => ({
+      type: 'SpeechSynthesisAction',
+      ...r,
+    })),
+    SpeechRecognitionAction.result.map(r => ({
+      type: 'SpeechRecognitionAction',
+      ...r,
+    })),
+  );
+  // TODO: update this
+  const inputC$ = PoseDetection.events('poses').mapTo({
+    val1: 0,
+    val2: 0,
+    val3: 0,
+  });
+  // const inputC$ = PoseDetection.events('poses').filter(poses =>
+  //     posses.length === 1
+  //     && poses[0].keypoints.filter(kpt => kpt.part === 'nose').length === 1
+  //   ).map(poses => {
+  //     const nose = poses[0].keypoints.filter(kpt => kpt.part === 'nose')[0];
+  //     return {
+  //       x: nose.position.x / 640,  // max value of position.x is 640
+  //       y: nose.position.y / 480,  // max value of position.y is 480
+  //     };
+  //   })
+  //   .compose(throttle(throttleDelay))
+  //   .compose(pairwise)
+  //   .map(([prev, cur]) => cur.x - prev.x)
+  //   .filter(delta => Math.abs(delta) > deltaThreshold)
+  //   .map(delta => ({type: InputType.MOVED_FACE, value: delta}));
+
+  return xs.merge(
+    inputD$.map(val => ({type: 'DISCRETE_INPUT', value: val})),
+    inputC$.map(val => ({type: 'CONTINUOUS_INPUT', value: val})),
+  );
 }
 
-// function transition(state, inputD, inputC) {
+function transition(prev, input) {
+  return prev;
+}
 
-// }
+function transitionReducer(input$) {
+  const initReducer$ = xs.of(
+    function initReducer(prev) {
+      return {
+        state: State.WAIT,
+        variables: {
+          goal_id: null,
+          newGoal: null,
+        },
+        outputs: null,
+      }
+    }
+  );
 
-function selectActionResult2(actionName) {
-  return (in$) => in$
-    .filter(s => !!s
-      && !!s[actionName]
-      && !!s[actionName].outputs
-      // && !!s[actionName].outputs.result
-      )
-    // .map(s => s[actionName].outputs.result)
-    // .compose(dropRepeats(isEqualResult));
+  const inputReducer$ = input$
+    .map(input => function inputReducer(prev) {
+      return transition(prev, input);
+    });
+
+  return xs.merge(initReducer$, inputReducer$);
 }
 
 function main(sources) {
-  // sources.state.stream.addListener({next: s => console.debug('reducer state', s)});
 
-  sources.state.stream.compose(selectActionResult2('FacialExpressionAction')).addListener({next: s => console.error('FacialExpression.result', s)});
 
-  input(sources)[0].addListener({next: v => console.warn(v)});
-  sources.FacialExpressionAction.result.addListener({next: v => console.error(v)});
+  const reducer = transitionReducer(input$);
 
-  // const goals$ = sources.TabletFace.events('load').mapTo({
-  const goals$ = xs.of({
+
+
+  input(sources)[0].addListener({next: v => console.debug(v)});
+
+  const goals$ = sources.TabletFace.events('load').mapTo({
     face: initGoal('HAPPY'),
     sound: initGoal('https://raw.githubusercontent.com/aramadia/willow-sound/master/G/G15.ogg'),
     robotSpeechbubble: initGoal('How are you?'),
     humanSpeechbubble: initGoal(['Good', 'Bad']),
     synthesis: initGoal('How are you?'),
     recognition: initGoal({}),
-  // });
-  }).compose(delay(1500));
+  });
 
   sources.HumanSpeechbubbleAction.result
     .addListener({next: result => {
