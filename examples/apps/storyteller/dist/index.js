fddb000649aa4484cd3ce56332f38a5b5c9c917b
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-04-10 00:39:35 -0700

    Remove storyteller app

--- examples/apps/storyteller/dist/index.js
+++
@@ -1,1 +0,0 @@
-console.error("\n/Users/mjyc/hcrlab/cycle-robot-drivers/examples/apps/storyteller/node_modules/@tensorflow/tfjs-converter/dist/src/data/compiled_api.ts:3\nexport declare interface IAny {\n^\nParseError: 'import' and 'export' may appear only with 'sourceType: module'");
