35d1df74f400bfe6298e37cc74a9e4fff4b22dbb
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-06-01 14:45:25 -0700

    Release sound 1.1.1

--- sound/src/index.ts
+++ sound/src/index.ts
@@ -2,5 +2,7 @@ export { makeAudioPlayerDriver } from "./makeAudioPlayerDriver";
 export {
   Sources as AudioPlayerActionSources,
   Sinks as AudioPlayerActionSinks,
+  status as selectAudioPlayerActionStatus,
+  output as selectAudioPlayerActionOutput,
   AudioPlayerAction
 } from "./AudioPlayerAction";
