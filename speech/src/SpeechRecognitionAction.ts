e214d9adf562d1320cb54453ea14518e63ffc1b2
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-06-01 14:42:35 -0700

    Expose status and output fncs

--- speech/src/SpeechRecognitionAction.ts
+++ speech/src/SpeechRecognitionAction.ts
@@ -247,7 +247,7 @@ function transitionReducer(input$: Stream<Input>): Stream<Reducer> {
   return xs.merge(initReducer$, inputReducer$);
 }
 
-function status(reducerState$): Stream<GoalStatus> {
+export function status(reducerState$): Stream<GoalStatus> {
   const done$: Stream<GoalStatus> = reducerState$
     .filter(rs => !!rs.outputs && !!rs.outputs.result)
     .map(rs => rs.outputs.result.status);
@@ -261,7 +261,7 @@ function status(reducerState$): Stream<GoalStatus> {
     .startWith(initGoalStatus);
 }
 
-function output(reducerState$) {
+export function output(reducerState$) {
   const outputs$ = reducerState$
     .filter(rs => !!rs.outputs)
     .map(rs => rs.outputs);
--- speech/src/index.ts
+++ speech/src/index.ts
@@ -2,11 +2,15 @@ export { makeSpeechSynthesisDriver } from "./makeSpeechSynthesisDriver";
 export {
   Sources as SpeechSynthesisActionSources,
   Sinks as SpeechSynthesisActionSinks,
+  status as selectSpeechSynthesisActionStatus,
+  output as selectSpeechSynthesisActionOutput,
   SpeechSynthesisAction
 } from "./SpeechSynthesisAction";
 export { makeSpeechRecognitionDriver } from "./makeSpeechRecognitionDriver";
 export {
   Sources as SpeechRecognitionActionSources,
   Sinks as SpeechRecognitionActionSinks,
+  status as selectSpeechRecognitionActionStatus,
+  output as selectSpeechRecognitionActionOutput,
   SpeechRecognitionAction
 } from "./SpeechRecognitionAction";
